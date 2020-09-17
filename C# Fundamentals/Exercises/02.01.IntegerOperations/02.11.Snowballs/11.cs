f43a60ee89311bb12d338c52ec45c761ba254de9
o-todorov <todorov.oleg44@gmail.com>
2020-09-17 12:16:09 +0100

    initial, scores 90%

---
+++ C# Fundamentals/Exercises/02.01.IntegerOperations/02.11.Snowballs/11.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02._11.Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //{snowballSnow} : {snowballTime} = {snowballValue} ({snowballQuality})
+            //(snowballSnow / snowballTime) ^ snowballQuality
+            int snowBallCount = int.Parse(Console.ReadLine());
+
+            string output = "";
+            double maxQuality = 0;
+
+            for (int i = 0; i < snowBallCount; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                double quality = Math.Pow(snowballSnow / snowballTime, snowballQuality);
+
+                if (quality > maxQuality)
+                {
+                    maxQuality = quality;
+                    output = $"{snowballSnow} : {snowballTime} = { quality} ({snowballQuality})";
+                }
+            }
+
+            Console.WriteLine(output);
+
+        }
+    }
+}
