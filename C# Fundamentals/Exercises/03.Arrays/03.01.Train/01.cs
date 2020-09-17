9f0e334a84c56296ca92940402ad47248c2b4327
o-todorov <todorov.oleg44@gmail.com>
2020-09-17 18:24:41 +0100

    added

---
+++ C# Fundamentals/Exercises/03.Arrays/03.01.Train/01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
