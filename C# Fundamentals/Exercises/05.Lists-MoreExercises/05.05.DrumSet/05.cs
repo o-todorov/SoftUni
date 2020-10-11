837b8a6a8d613b151ec75436d8972d6a7c6f0545
o-todorov <todorov.oleg44@gmail.com>
2020-10-11 23:28:24 +0100

    initial 100%

---
+++ C# Fundamentals/Exercises/05.Lists-MoreExercises/05.05.DrumSet/05.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _05._05.DrumSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+
+            List<int> startDrums = Console.ReadLine().Split(" ")
+                                    .Select(int.Parse)
+                                    .ToList();
+            List<int> drums =new List<int>( startDrums.ToArray());
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(input);
+
+                for (int i = 0; i < drums.Count; i++)
+                {
+                    drums[i] -= hitPower;
+
+                    if (drums[i] <= 0)
+                    {
+                        if (savings >= startDrums[i] * 3)
+                        {
+                            drums[i] = startDrums[i];
+                            savings -= startDrums[i] * 3;
+                        }
+                        else
+                        {
+                            drums.RemoveAt(i);
+                            startDrums.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", drums));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+
+        }
+
+    }
+}
