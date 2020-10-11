1652947d55e1c0ca72520ee9bcee62422a419831
o-todorov <todorov.oleg44@gmail.com>
2020-10-11 19:26:45 +0100

    initial 50%

---
+++ C# Fundamentals/Exercises/05.Lists-MoreExercises/05.04.MixedUpLists/04.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._04.MixedUpLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToList();
+
+            List<int> secondList = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToList();
+
+            if(secondList.Count>firstList.Count)
+            {
+                firstList.AddRange(secondList.GetRange( secondList.Count - 2, 2));
+                secondList.RemoveRange(secondList.Count - 2, 2);
+            }
+
+            List<int> result = new List<int>();
+
+            int idx = secondList.Count;
+
+            for (int i = 0; i < idx; i++)
+            {
+                result.Add(firstList[i]);
+                result.Add(secondList[idx - i - 1]);
+            }
+
+            result.Sort();
+
+            int start   = Math.Min(firstList[idx], firstList[idx + 1]);
+            int end     = Math.Max(firstList[idx], firstList[idx + 1]);
+
+            Console.WriteLine(string.Join(" ", result.Where(c => c > start && c < end)));
+
+        }
+    }
+}
