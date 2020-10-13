c17b55572704168e5de34f3ff7b2e9ead8dbc67c
o-todorov <todorov.oleg44@gmail.com>
2020-10-14 00:09:09 +0100

    initial 100%

--- C# Fundamentals/Exercises/06.ObjectsandClasses/06.03.Articles2.0/03.cs
+++ C# Fundamentals/Exercises/06.ObjectsandClasses/06.03.Articles2.0/03.cs
@@ -57,11 +57,6 @@ namespace _06._03.Articles2._0
         public string Title { get; set; }
         public string Content { get; set; }
         public string Author { get; set; }
-        public void Edit(string newContent) { Content = newContent; }
-        public void ChangeAuthor(string newAuthor) { Author = newAuthor; }
-
-        public void Rename(string newTitle) { Title = newTitle; }
-
         public override string ToString()
         {
             return $"{Title} - { Content}: {Author}";
---
+++ C# Fundamentals/Exercises/06.ObjectsandClasses/06.04.Students/04.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._04.Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsCount = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                string[] article = Console.ReadLine().Split(" ");
+
+                students.Add(new Student(article[0], article[1], double.Parse(article[2])));
+            }
+
+            var res = students.OrderByDescending(st => st.Grade);
+
+            foreach (var st in res)
+            {
+                Console.WriteLine(st.ToString());
+            }
+        }
+    }
+
+
+    class Student
+    {
+        public Student(string _FirstName, string _LastName, double _Grade)
+        {
+            this.FirstName = _FirstName;
+            this.LastName = _LastName;
+            this.Grade = _Grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} { LastName}: {Grade:f2}";
+        }
+
+    }
+
+}
