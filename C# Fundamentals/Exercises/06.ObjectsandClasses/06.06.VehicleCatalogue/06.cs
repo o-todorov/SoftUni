e08c95eeb24d169b044cf5a56eaaec2bc19807f5
o-todorov <todorov.oleg44@gmail.com>
2020-10-16 22:24:48 +0100

    initial 100%

---
+++ C# Fundamentals/Exercises/06.ObjectsandClasses/06.06.VehicleCatalogue/06.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._06.VehicleCatalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            string newVehicle = string.Empty;
+
+            while ((newVehicle = Console.ReadLine()).ToUpper() != "END")
+            {
+                vehicles.Add(new Vehicle(newVehicle));
+            }
+
+            string model = string.Empty;
+
+            while ((model = Console.ReadLine()).ToLower() != "close the catalogue")
+            {
+                vehicles.Find(v => v.Model == model).Print();
+            }
+
+            PrintAverageHPower(ref vehicles, "CAR", "Cars");
+            PrintAverageHPower(ref vehicles, "TRUCK", "Trucks");
+
+        }
+
+        private static void PrintAverageHPower(ref List<Vehicle> vehicles, string vType, string heading)
+        {
+            var veh = vehicles.Where(v => v.Type.ToUpper() == vType).ToList();
+            double averageHPower = 0.00;
+
+            if (veh.Count > 0)
+            {
+                int hPowers = 0;
+                veh.ForEach(v => hPowers += v.Horsepower);
+                averageHPower = (double)hPowers / veh.Count;
+            }
+
+            Console.WriteLine($"{heading} have average horsepower of: {averageHPower:f2}.");
+        }
+    }
+
+
+    class Vehicle
+    {
+        public string   Type;
+        public string   Model;
+        public string   Color;
+        public int      Horsepower;
+
+        public Vehicle(string vehicleSpec)
+        {
+            string[] vehicle = vehicleSpec.Split();
+
+            this.Type   = vehicle[0];
+            this.Model  = vehicle[1];
+            this.Color  = vehicle[2];
+            this.Horsepower = int.Parse(vehicle[3]);
+        }
+
+        public void Print()
+        {
+            var charArr = Type.ToCharArray();
+            charArr[0]  = char.ToUpper(charArr[0]);
+
+            Console.WriteLine($"Type: {new string(charArr)}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Color: {Color }");
+            Console.WriteLine($"Horsepower: {Horsepower }");
+        }
+    }
+}
---
+++ C# Fundamentals/Exercises/06.ObjectsandClasses/06.07.OrderByAge/07.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._07.OrderByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            string input;
+
+            while ((input=Console.ReadLine()).ToUpper()!="END")
+            {
+                string[] person = input.Split();
+                persons.Add(new Person(person));
+            }
+
+            persons.OrderBy(p => p.Age).ToList().ForEach(p => p.Print());
+
+        }
+    }
+
+    class Person
+    {
+        public string   Name;
+        public string   ID;
+        public int      Age;
+
+        public Person(string[] nameIdAge)
+        {
+            this.Name   = nameIdAge[0];
+            this.ID     = nameIdAge[1];
+            this.Age    = int.Parse(nameIdAge[2]);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} with ID: {ID} is {Age} years old.");
+        }
+    }
+}
