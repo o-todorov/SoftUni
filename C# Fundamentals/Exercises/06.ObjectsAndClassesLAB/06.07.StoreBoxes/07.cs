4cad71c8d9a2925430cb441d0d4e0bbc9fc770af
o-todorov <todorov.oleg44@gmail.com>
2020-10-25 16:20:03 +0000

    initian upload 100%

---
+++ C# Fundamentals/Exercises/06.ObjectsAndClassesLAB/06.07.StoreBoxes/07.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _06._07.StoreBoxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boxes = new List<Box>();
+
+            string[] input;
+
+            while ((input = Console.ReadLine().Split())[0] != "end")
+            {
+                string  serNum      = input[0];
+                string  item        = input[1];
+                int     quant       = int.Parse(input[2]);
+                double  itemPrice   = double.Parse(input[3]);
+
+                boxes.Add(new Box(serNum, new Item(item, itemPrice), quant));
+            }
+
+            boxes.OrderByDescending(b => b.Price)
+                .ToList()
+                .ForEach(b => Console.Write(b));
+
+        }
+    }
+
+    class Item
+    {
+        public Item(string name, double price)
+        {
+            Name    = name;
+            Price   = price;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Box
+    {
+        public Box(string serialNum, Item item, int itemQuantity)
+        {
+            SerialNum       = serialNum;
+            Item            = item;
+            ItemQuantity    = itemQuantity;
+        }
+
+        public string   SerialNum { get; set; }
+
+        public Item     Item { get; set; }
+
+        public int      ItemQuantity { get; set; }
+
+        public double   Price { get { return Item.Price * ItemQuantity; } }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(SerialNum);
+            sb.AppendLine($"-- {Item.Name} - ${Item.Price:f2}: {ItemQuantity}");
+            sb.AppendLine($"-- ${Price:f2}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# Fundamentals/Exercises/06.ObjectsAndClassesLAB/06.08.VehicleCatalog/08.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace _06._08.VehicleCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var catalog = new Catalog();
+            string[] input;
+
+            while ((input=Console.ReadLine().Split('/'))[0] != "end")
+            {
+                string type     = input[0];
+                string brand    = input[1];
+                string model    = input[2];
+                int horsePower  = int.Parse( input[3]);
+                int weight      = int.Parse(input[3]);
+
+                if (type == "Car")
+                {
+                    catalog.Add(new Car(brand, model, horsePower));
+                }
+                else if (type == "Truck")
+                {
+                    catalog.Add(new Truck(brand, model, weight));
+                }
+            }
+
+            catalog.Print("cars");
+            catalog.Print("trucks");
+        }
+
+
+    }
+
+    class Catalog
+    {
+        private List<Truck>  Trucks;
+        private List<Car>    Cars;
+
+        public Catalog()
+        {
+            Trucks  = new List<Truck>();
+            Cars    = new List<Car>();
+        }
+
+        public void Add(Car veh) 
+        {
+            Cars.Add(veh);
+        }
+        public void Add(Truck veh) 
+        {
+            Trucks.Add(veh);
+        }
+
+        public void Print(string vehicleType)
+        {
+            if (vehicleType.ToLower() == "cars" && Cars.Count > 0)
+            {
+                Console.WriteLine("Cars:");
+                Console.WriteLine(string.Join(Environment.NewLine, Cars.OrderBy(c => c.Brand)));
+            }
+            else if (vehicleType.ToLower() == "trucks" && Trucks.Count > 0)
+            {
+                Console.WriteLine("Trucks:");
+                Console.WriteLine(string.Join(Environment.NewLine, Trucks.OrderBy(c => c.Brand)));
+            }
+
+        }
+    }
+
+    class Truck
+    {
+        public Truck(string brand, string model, int weight)
+        {
+            Brand   = brand;
+            Model   = model;
+            Weight  = weight;
+        }
+
+        public string   Brand { get; set; }
+        public string   Model { get; set; }
+        public int      Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Brand}: {Model} - {Weight}kg";
+        }
+    }
+
+    class Car
+    {
+        public Car(string brand, string model, int horsePower)
+        {
+            Brand       = brand;
+            Model       = model;
+            HorsePower  = horsePower;
+        }
+
+        public string   Brand { get; set; }
+        public string   Model { get; set; }
+        public int      HorsePower { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Brand}: {Model} - {HorsePower}hp";
+        }
+    }
+
+}
