69b2569e7e9624d6e845c4e848d0a52f8d332429
o-todorov <todorov.oleg44@gmail.com>
2020-10-27 21:06:37 +0000

    initial 100%

---
+++ C# Fundamentals/Exercises/07.AssociativeArrays/07.07.StudentAcademy/07.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._07.StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new Dictionary<string, List<double>>();
+
+            int studCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < studCount; i++)
+            {
+                string stName = Console.ReadLine();
+                double stGrade = double.Parse(Console.ReadLine());
+
+                if (students.ContainsKey(stName))
+                {
+                    students[stName].Add(stGrade);
+                }
+                else
+                {
+                    students[stName] = new List<double>() { stGrade };
+                }
+            }
+
+            foreach (var stud in students.ToDictionary(p => p.Key, p => p.Value.Average())
+                                         .Where(p => p.Value >= 4.50)
+                                         .OrderByDescending(p => p.Value))
+            {
+                Console.WriteLine($"{stud.Key} -> {stud.Value:f2}");
+            }
+        }
+
+    }
+
+}
