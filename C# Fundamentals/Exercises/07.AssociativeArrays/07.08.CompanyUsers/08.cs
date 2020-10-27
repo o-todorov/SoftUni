64c767dcb52337f62dc59cbccd731360ba6f5eaa
o-todorov <todorov.oleg44@gmail.com>
2020-10-27 22:37:03 +0000

    initial 100%

---
+++ C# Fundamentals/Exercises/07.AssociativeArrays/07.08.CompanyUsers/08.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._08.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var companies = new SortedDictionary<string, List<string>>();
+
+            string[] input;
+
+            while ((input = Console.ReadLine().Split(" -> "))[0] != "End")
+            {
+                string compName = input[0];
+                string empID    = input[1];
+                
+                if (companies.ContainsKey(compName))
+                {
+                    if (!companies[compName].Contains(empID))
+                    {
+                        companies[compName].Add(empID);
+                    }
+                }
+                else
+                {
+                    companies[compName] = new List<string>() { empID };
+                }
+            }
+
+            foreach (var comp in companies)
+            {
+                Console.WriteLine($"{comp.Key}");
+
+                comp.Value.ForEach(c => Console.WriteLine($"-- {c}"));
+            }
+
+
+        }
+    }
+}
