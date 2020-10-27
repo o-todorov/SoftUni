2d46f01317b4deb33506ef1c996161c5e0d2b4d6
o-todorov <todorov.oleg44@gmail.com>
2020-10-27 20:24:29 +0000

    initial 100%

--- C# Fundamentals/Exercises/07.AssociativeArrays/07.05.SoftUniParking/05.cs
+++ C# Fundamentals/Exercises/07.AssociativeArrays/07.05.SoftUniParking/05.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace _07._05.SoftUniParking
 {
---
+++ C# Fundamentals/Exercises/07.AssociativeArrays/07.06.Courses/06.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._06.Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var courses = new Dictionary<string, List<string>>();
+
+            string[] input;
+
+            while ((input=Console.ReadLine().Split(" : "))[0] != "end")
+            {
+                string course   = input[0];
+                string student  = input[1];
+
+                if (!courses.ContainsKey(course))
+                {
+                    courses.Add(course, new List<string>() { student });
+                }
+                else
+                {
+                    courses[course].Add(student);
+                }
+            }
+
+            foreach(var c in courses.OrderByDescending(c => c.Value.Count))
+            {
+                Console.WriteLine($"{c.Key}: {c.Value.Count}");
+
+                c.Value.OrderBy(s=>s)
+                        .ToList()
+                        .ForEach(s => Console.WriteLine($"-- {s}"));
+            }
+
+
+        }
+    }
+
+    
+}
