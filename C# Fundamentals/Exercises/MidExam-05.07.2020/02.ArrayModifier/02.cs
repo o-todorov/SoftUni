a4b7c7243142c0daf1debde45d2bb371188749d6
o-todorov <todorov.oleg44@gmail.com>
2020-10-30 02:13:08 +0000

    initial

---
+++ C# Fundamentals/Exercises/MidExam-05.07.2020/02.ArrayModifier/02.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.ArrayModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                       .Split(" ")
+                       .Select(int.Parse).ToArray();
+
+            string[] commArr;
+
+            while ((commArr = Console.ReadLine().Split())[0].ToLower() != "end")
+            {
+                string command = commArr[0];
+
+                if (command == "swap")
+                {
+                    int idx1 = int.Parse(commArr[1]);
+                    int idx2 = int.Parse(commArr[2]);
+                    int tmp = arr[idx1];
+                    arr[idx1] = arr[idx2];
+                    arr[idx2] = tmp;
+                }
+                else if (command == "multiply")
+                {
+                    int idx1 = int.Parse(commArr[1]);
+                    int idx2 = int.Parse(commArr[2]);
+                    arr[idx1] = arr[idx1] * arr[idx2];
+                }
+                else if (command == "decrease")
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i] -= 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", arr));
+
+        }
+    }
+}
