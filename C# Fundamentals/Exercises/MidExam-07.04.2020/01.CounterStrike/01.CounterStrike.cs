8a474e38dacd7cb95d380b033e0b25e6a66decd0
o-todorov <todorov.oleg44@gmail.com>
2020-11-01 23:30:44 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/MidExam-07.04.2020/01.CounterStrike/01.CounterStrike.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01.CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+            int wonBattles = 0;
+
+            while ((input = Console.ReadLine()).ToLower() != "end of battle")
+            {
+                int distance = int.Parse(input);
+
+                if (distance > energy)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wonBattles} won battles and {energy} energy");
+                    return;
+                }
+                else
+                {
+                    energy -= distance;
+                    wonBattles++;
+                    if (wonBattles % 3 == 0)
+                    {
+                        energy += wonBattles;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Won battles: {wonBattles}. Energy left: {energy}");
+        }
+    }
+}
