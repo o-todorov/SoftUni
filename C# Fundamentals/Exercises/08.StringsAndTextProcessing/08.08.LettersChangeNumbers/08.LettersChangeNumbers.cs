3036a9bbf99a87a087449368b581c3b28c20571d
o-todorov <todorov.oleg44@gmail.com>
2020-11-30 20:53:09 +0000

    initial upload 90%

---
+++ C# Fundamentals/Exercises/08.StringsAndTextProcessing/08.08.LettersChangeNumbers/08.LettersChangeNumbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _08._08.LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            double total = 0.00;
+
+            foreach (var str in input)
+            {
+                int number = int.Parse(str.Substring(1, str.Length - 2));
+                double temp = 0.00;
+                char letter = str[0];
+                
+                if (char.IsUpper(letter))
+                {
+                    int position = letter - 'A' + 1;
+                    temp = (double)number / position;
+                }
+                else
+                {
+                    int position = letter - 'a' + 1;
+                    temp = number * position;
+                }
+
+                letter = str[str.Length-1];
+                
+                if (char.IsUpper(letter))
+                {
+                    int position = letter - 'A' + 1;
+                    temp -=  position;
+                }
+                else
+                {
+                    int position = letter - 'a' + 1;
+                    temp +=  position;
+                }
+
+                total += temp;
+            }
+
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
