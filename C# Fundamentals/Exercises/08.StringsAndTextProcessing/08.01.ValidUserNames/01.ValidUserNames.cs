7a18b6d8f3bef31a168bfdc46d05f4b44099cde9
o-todorov <todorov.oleg44@gmail.com>
2020-11-30 20:49:25 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/08.StringsAndTextProcessing/08.01.ValidUserNames/01.ValidUserNames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._01.ValidUserNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(s => s.Length >= 3 && s.Length <= 16)
+                .Where(s => s.All(c => Char.IsLetterOrDigit(c) || c == '-' || c == '_')).ToArray();
+
+            foreach (var item in input)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
