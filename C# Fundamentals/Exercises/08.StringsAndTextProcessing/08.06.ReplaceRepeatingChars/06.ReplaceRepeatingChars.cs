6067dc65cb0540bd76d51d53191812877df78c2f
o-todorov <todorov.oleg44@gmail.com>
2020-11-30 20:50:47 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/08.StringsAndTextProcessing/08.06.ReplaceRepeatingChars/06.ReplaceRepeatingChars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace _08._06.ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var sb = new StringBuilder();
+
+            char ch = input[0];
+            sb.Append(ch);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == ch)
+                {
+                    continue;
+                }
+
+                ch = input[i];
+                sb.Append(ch);
+            }
+
+            Console.WriteLine(sb.ToString());
+
+
+        }
+    }
+}
