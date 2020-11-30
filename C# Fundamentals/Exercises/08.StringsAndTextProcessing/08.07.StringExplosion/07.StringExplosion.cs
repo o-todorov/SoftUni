66b2bd9ecfa8dcddbb07bcd3f4aee9ad11ac7433
o-todorov <todorov.oleg44@gmail.com>
2020-11-30 20:51:13 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/08.StringsAndTextProcessing/08.07.StringExplosion/07.StringExplosion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace _08._07.StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int strength = 0;
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += input[i++ + 1] - '1';
+                    sb.Append('>');
+                    continue;
+                }
+
+                if (strength > 0)
+                {
+                    strength--;
+                    continue;
+                }
+
+                sb.Append(input[i]);
+            }
+
+            Console.WriteLine(sb.ToString());
+
+
+        }
+    }
+}
