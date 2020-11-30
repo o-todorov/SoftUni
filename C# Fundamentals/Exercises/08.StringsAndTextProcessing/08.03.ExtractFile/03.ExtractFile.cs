968911e53c1eece6c225041e139d3cd6d15a7376
o-todorov <todorov.oleg44@gmail.com>
2020-11-30 20:49:58 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/08.StringsAndTextProcessing/08.03.ExtractFile/03.ExtractFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08._03.ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int i = input.LastIndexOf(@"\") + 1;
+            int j = input.LastIndexOf(".");
+
+            Console.WriteLine($"File name: {input.Substring(i, j - i)}");
+            Console.WriteLine($"File extension: {input.Substring(j + 1)}");
+
+        }
+    }
+}
+
