716e544f8b1110a2c664620faa1c70e31beca520
o-todorov <todorov.oleg44@gmail.com>
2020-11-30 20:50:14 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/08.StringsAndTextProcessing/08.04.CaesarCipher/04.CaesarCipher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._04.CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var result = input.Select(ch=>(char)(ch+3));
+
+            Console.WriteLine(string.Join("",result));
+        }
+    }
+}
