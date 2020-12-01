6cea129aa1247adecc7ee1e4beb17532b08b96fe
o-todorov <todorov.oleg44@gmail.com>
2020-12-01 22:34:13 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/08.StringsandTextProcessing-MoreExercises/08.03.TreasureFinder/03.TreasureFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _08._03.TreasureFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                       .Split(" ")
+                       .Select(int.Parse).ToArray();
+
+            string input = Console.ReadLine();
+
+            while (input != "find")
+            {
+                var sb = new StringBuilder();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    sb.Append((char)(input[i] - nums[i % nums.Length]));
+                }
+
+                string item         = GetSubStr(sb.ToString(), '&', '&');
+                string coordinates  = GetSubStr(sb.ToString(), '<', '>');
+                Console.WriteLine($"Found {item} at {coordinates}");
+
+                input = Console.ReadLine();
+            }
+        }
+        private static string GetSubStr(string input, char v1, char v2)
+        {
+            int i = input.IndexOf(v1) + 1;
+            int j = input.IndexOf(v2, i);
+
+            return input.Substring(i, j - i);
+        }
+
+    }
+}
