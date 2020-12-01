9e96f6956dcaecaaad6a20251e4036df9d87b23d
o-todorov <todorov.oleg44@gmail.com>
2020-12-01 22:34:23 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/08.StringsandTextProcessing-MoreExercises/08.04.MorseCodeTranslator/04.MorseCodeTranslator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._04.MorseCodeTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, char> dict = new Dictionary<string, char>()
+            {
+                {".-",'A' },    {"-...",'B' },  {"-.-.",'C' },  {"-..",'D' },   {".",'E' },     {"..-.",'F' },
+                {"--.",'G' },   {"....",'H' },  {"..",'I' },    {".---",'J' },  {"-.-",'K' },   {".-..",'L' },
+                {"--",'M' },    {"-.",'N' },    {"---",'O' },   {".--.",'P' },  {"--.-",'Q' },  {".-.",'R' },
+                {"...",'S' },   {"-",'T' },     {"..-",'U' },   {"...-",'V' },  {".--",'W' },   {"-..-",'X' },
+                {"-.--",'Y' },  {"--..",'Z' }
+            };
+
+            foreach (var word in words) 
+            {
+                var w = new string(word.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(ch => dict[ch])
+                            .ToArray());
+
+                Console.Write(w);
+                Console.Write(" ");
+            }
+        }
+    }
+}
