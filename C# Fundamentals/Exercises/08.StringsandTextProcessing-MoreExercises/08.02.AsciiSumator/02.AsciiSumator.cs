0f56cd5754e5d957c20ea69b8e497e751be47cf2
o-todorov <todorov.oleg44@gmail.com>
2020-12-01 22:33:06 +0000

    initial upload

---
+++ C# Fundamentals/Exercises/08.StringsandTextProcessing-MoreExercises/08.02.AsciiSumator/02.AsciiSumator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08._02.AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int     a       = char.Parse(Console.ReadLine());
+            int     b       = char.Parse(Console.ReadLine());
+            string  input   = Console.ReadLine();
+
+            if (b < a)
+            {
+                int t = a;
+                a = b;
+                b = t;
+            }
+
+            int totalSum = 0;
+
+            foreach (var ch in input)
+            {
+                if (ch > a & ch < b)
+                {
+                    totalSum += ch;
+                }
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
