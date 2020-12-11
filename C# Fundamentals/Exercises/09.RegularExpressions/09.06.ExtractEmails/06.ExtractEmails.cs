126f8f3e44ce272baef9c38453020339081d350a
o-todorov <todorov.oleg44@gmail.com>
2020-12-11 20:45:05 +0200

    initial upload

---
+++ C# Fundamentals/Exercises/09.RegularExpressions/09.06.ExtractEmails/06.ExtractEmails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _09._06.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rgxEmailPattern = @"(?<=[ ,])[a-zA-Z0-9]+([.\-_][a-zA-Z0-9]+)*@[a-zA-Z]+([\.-][a-zA-Z]+)*(?=\.[a-zA-Z])(\.[a-zA-Z]+)";
+            string input = Console.ReadLine();
+
+            foreach (Match email in Regex.Matches(input,rgxEmailPattern))
+            {
+                Console.WriteLine(email.Value);
+            }
+
+        }
+    }
+}
