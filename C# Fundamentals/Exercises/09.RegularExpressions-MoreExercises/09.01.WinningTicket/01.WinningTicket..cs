afd8102242d469cc34bc56201733b1f2a4a03e85
o-todorov <todorov.oleg44@gmail.com>
2020-12-11 20:48:13 +0200

    initial upload

---
+++ C# Fundamentals/Exercises/09.RegularExpressions-MoreExercises/09.01.WinningTicket/01.WinningTicket..cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _09._01.WinningTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tickets = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            char[] symbols = { '@', '#', '$', '^' };
+
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                bool success = false;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    string rgx = $"[\\{symbols[i]}]{{6,10}}";
+
+                    var matchLeft   = Regex.Match(ticket.Substring(0, 10), rgx);
+                    var matchRight  = Regex.Match(ticket.Substring(10), rgx);
+
+                    if (matchRight.Success && matchRight.Success)
+                    {
+                        success = true;
+
+                        if (matchLeft.Length == 10 && matchRight.Length == 10)
+                        {
+                            Console.WriteLine($"ticket \"{ ticket}\" - {matchLeft.Length}{symbols[i]} Jackpot!");
+                            break;
+                        }
+                        else
+                        {
+                            int minCount = Math.Min(matchLeft.Length, matchRight.Length);
+                            Console.WriteLine($"ticket \"{ ticket}\" - {minCount}{symbols[i]}");
+                            break;
+                        }
+                    }
+                }
+
+                if (!success)
+                {
+                    Console.WriteLine($"ticket \"{ ticket}\" - no match");
+                }
+            }
+        }
+    }
+}
+
+// (?=.{20}).*?(?=(?<ch>[@#$^]))(?<match>\k<ch>{6,}).*(?<=.{10})\k<match>.*   REGEX to validate the ticket
