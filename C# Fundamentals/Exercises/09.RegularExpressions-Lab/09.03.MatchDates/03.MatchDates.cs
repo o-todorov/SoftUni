5a2b04966efd3f1ad1191b27020be69f1059c339
o-todorov <todorov.oleg44@gmail.com>
2020-12-11 20:48:02 +0200

    initial upload

---
+++ C# Fundamentals/Exercises/09.RegularExpressions-Lab/09.03.MatchDates/03.MatchDates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _09._03.MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b(?<day>((0|1|2)\d)|3(0|1))([-.\/])(?<month>[JFMASOND][a-z]{2})\4(?<year>\d{4})\b";
+            string input = Console.ReadLine();
+            MatchCollection dates = Regex.Matches(input, regex);
+
+            foreach (Match date in dates)
+            {
+                var day     = date.Groups["day"].Value;
+                var month   = date.Groups["month"].Value;
+                var year    = date.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+
+        }
+    }
+}
