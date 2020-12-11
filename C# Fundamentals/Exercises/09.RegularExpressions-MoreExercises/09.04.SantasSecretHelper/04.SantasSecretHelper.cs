a18314f1351977779a56990091a7afcaea46b57e
o-todorov <todorov.oleg44@gmail.com>
2020-12-11 20:48:38 +0200

    initial upload

---
+++ C# Fundamentals/Exercises/09.RegularExpressions-MoreExercises/09.04.SantasSecretHelper/04.SantasSecretHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _09._04.SantasSecretHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int code = int.Parse(Console.ReadLine());
+            string input;
+            Regex regex = new Regex(@"@(?<name>[A-Za-z]+)[^@\-!:>]*!(?<behaviour>[GN]!)");
+            Match match;
+
+            while ((input=Console.ReadLine())!="end")
+            {
+                var decoded =input.Select(ch =>(char) (ch - code)).ToArray();
+                match = regex.Match(new string(decoded));
+
+                if (match.Success&& match.Groups["behaviour"].Value[0]=='G')
+                {
+                    Console.WriteLine($"{match.Groups["name"].Value}");
+                }
+            }
+        }
+    }
+}
