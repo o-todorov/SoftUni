c66ab5ea9870fa4ea1874eb4e15980131dadf13e
o-todorov <todorov.oleg44@gmail.com>
2020-12-11 20:47:16 +0200

    initial upload

---
+++ C# Fundamentals/Exercises/09.RegularExpressions-Lab/09.02.MatchPhoneNumber/02.MatchPhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _09._02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"(?<= |^)\+359( |-)2\1\d{3}\1\d{4}\b";
+            string input = Console.ReadLine();
+            MatchCollection phones = Regex.Matches(input, regex);
+
+            Console.WriteLine(string.Join(", ",phones));
+
+        }
+    }
+}
