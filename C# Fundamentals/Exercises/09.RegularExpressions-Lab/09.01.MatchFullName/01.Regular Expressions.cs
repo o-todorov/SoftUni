239a0f9a62ce09f53dd6a7fb47e1379b77d38208
o-todorov <todorov.oleg44@gmail.com>
2020-12-11 20:47:08 +0200

    initial upload

---
+++ C# Fundamentals/Exercises/09.RegularExpressions-Lab/09.01.MatchFullName/01.Regular Expressions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _09._01.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"(?<!\w)[A-Z][a-z]+ [A-Z][a-z]+";
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, regex);
+
+            foreach (var name in matches)
+            {
+                Console.Write(name + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
