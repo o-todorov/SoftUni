ced38085a5a1c5e8c067ff38d7b15bf945df9731
o-todorov <todorov.oleg44@gmail.com>
2020-12-29 00:23:13 +0200

    08.Trafic Jam added

---
+++ C# Advanced/Exercises/01.StacksAndQueues/01.08.TrafficJam/08.TrafficJam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsToPass = int.Parse(Console.ReadLine());
+            int counter = 0;
+            Queue<string> waitingCars = new Queue<string>();
+            string input = Console.ReadLine();
+
+            while (input!="end")
+            {
+                if (input=="green")
+                {
+                    for (int i = Math.Min(waitingCars.Count, carsToPass); i > 0; i--)
+                    {
+                        Console.WriteLine($"{waitingCars.Dequeue()} passed!");
+                        counter++;
+                    }
+                }
+                else
+                {
+                    waitingCars.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
