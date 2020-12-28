275a9aeae59934e82cf93c0240dfc57e1810ead1
o-todorov <todorov.oleg44@gmail.com>
2020-12-29 00:22:48 +0200

    07.Hot Potato added

---
+++ C# Advanced/Exercises/01.StacksAndQueues/01.07.HotPotato/07.HotPotato.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._07.HotPotato
+{
+    class HotPotato
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Queue<string> players = new Queue<string>();
+
+            foreach (var item in input)
+            {
+                players.Enqueue(item);
+            }
+
+            int jump = int.Parse(Console.ReadLine());
+
+            while (players.Count > 1)
+            {
+                for (int i = 0; i < jump - 1; i++)
+                {
+                    players.Enqueue(players.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {players.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {players.Dequeue()}");
+        }
+    }
+}
