c4cfb489508998b5d58e6f44bfa381a44a59f6d4
o-todorov <todorov.oleg44@gmail.com>
2020-12-29 00:22:26 +0200

    04.Matching Brackets added

---
+++ C# Advanced/Exercises/01.StacksAndQueues/01.04.MatchingBrackets/04.StacksAndQueues.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+            string expression = Console.ReadLine();
+            Stack<int> brackets = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    brackets.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int n = brackets.Pop();
+                    Console.WriteLine(expression.Substring(n, i - n + 1));
+                }
+            }
+
+        }
+    }
+}
