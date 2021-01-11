81a7c6b4453ab8a655fc20c37d66c8b1b12b9270
o-todorov <todorov.oleg44@gmail.com>
2021-01-12 01:36:36 +0200

    Fashion Boutique upload

---
+++ C# Advanced/Exercises/01.StacksAndQueuesExercises/01.05.FashionBoutique/05.FashionBoutique.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._05.FashionBoutique
+{
+    class FashionBoutique
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> box = new Stack<int>(Console.ReadLine()
+                       .Split(" ")
+                       .Select(int.Parse).ToArray());
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int racksUsed = 1;
+            int freeSpace = rackCapacity;
+
+            while (box.Count!=0)
+            {
+                if (box.Peek() <= freeSpace)
+                {
+                    freeSpace -= box.Pop();
+                }
+                else
+                {
+                    freeSpace = rackCapacity - box.Pop();
+                    racksUsed++;
+                }
+            }
+
+            Console.WriteLine(racksUsed);
+        }
+    }
+}
