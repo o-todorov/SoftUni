ce2b6cc8220324dda61253622420b81e8d582fd5
o-todorov <todorov.oleg44@gmail.com>
2021-01-12 01:36:12 +0200

    Fast Food upload

---
+++ C# Advanced/Exercises/01.StacksAndQueuesExercises/01.04.FastFood/04.FastFood.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine()
+                        .Split(" ")
+                        .Select(int.Parse)
+                        .ToArray();
+
+            Console.WriteLine(orders.Max());
+            Queue<int> queue = new Queue<int>(orders);
+
+            while (queue.Count > 0 && food >= queue.Peek())
+            {
+                food -= queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: { string.Join(" ", queue.ToArray())}");
+            }
+
+        }
+    }
+}
