28b576d83552d9c136c219469aa112b9832fb014
o-todorov <todorov.oleg44@gmail.com>
2021-01-12 22:35:58 +0200

    Simple Text Editor upload

---
+++ C# Advanced/Exercises/01.StacksAndQueuesExercises/01.09.SimpleTextEditor/09.SimpleTextEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._09.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opsCount = int.Parse(Console.ReadLine());
+            Stack<string> memo = new Stack<string>();
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < opsCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", 2);
+
+                switch (input[0])
+                {
+                    case "1":
+                        memo.Push(sb.ToString());
+                        sb.Append(input[1]);
+                        break;
+                    case "2":
+                        memo.Push(sb.ToString());
+                        int charsToErase = int.Parse(input[1]);
+                        sb.Remove(sb.Length - charsToErase, charsToErase);
+                        break;
+                    case "3":
+                        int idx = int.Parse(input[1]) - 1;
+                        Console.WriteLine(sb[idx]); ;
+                        break;
+                    case "4":
+                        sb=new StringBuilder(memo.Pop());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
