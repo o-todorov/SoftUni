bb7fb17f63f345d0036332d3edb7b40a6fdc5b11
o-todorov <todorov.oleg44@gmail.com>
2021-01-13 01:19:40 +0200

    Diagonal Difference upload

---
+++ C# Advanced/Exercises/02.Multidimensional_Arrays/02.01.DiagonalDifference/01.DiagonalDifference.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    matrix[i, j] = arr[j];
+                }
+            }
+
+            int sumA = 0;
+            int sumB = 0;
+
+            for (int i = 0, j = matrixSize - 1; i < matrixSize; i++, j--)
+            {
+                sumA += matrix[i, j];
+                sumB += matrix[i, i];
+            }
+
+            Console.WriteLine(Math.Abs(sumA - sumB));
+
+        }
+    }
+}
