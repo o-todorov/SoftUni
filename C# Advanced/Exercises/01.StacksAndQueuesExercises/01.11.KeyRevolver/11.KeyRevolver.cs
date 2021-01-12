0a0b73e96422b670c7da253d4cf4b50e320cc9c3
o-todorov <todorov.oleg44@gmail.com>
2021-01-12 22:36:36 +0200

    Key Revolver upload

---
+++ C# Advanced/Exercises/01.StacksAndQueuesExercises/01.11.KeyRevolver/11.KeyRevolver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._11.KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+            int barrel = barrelSize;
+
+            Queue<int> bullets = new Queue<int>(Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .Reverse()
+                    .ToArray());
+            Queue<int> locks = new Queue<int>(Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray());
+            int budget = int.Parse(Console.ReadLine());
+
+            while (bullets.Count > 0 && locks.Count > 0)
+            {
+                if (bullets.Dequeue() <= locks.Peek())
+                {
+                    locks.Dequeue();
+                    Console.WriteLine("Bang!");
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                budget -= bulletPrice;
+                barrel--;
+
+                if (barrel == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                    barrel = barrelSize;
+                }
+            }
+
+            if (locks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${budget}");
+            }
+
+        }
+    }
+}
