e7c7c5a918f25be2145711d3e13d1c06ab860d88
o-todorov <todorov.oleg44@gmail.com>
2021-01-20 00:46:45 +0200

    Sets Of Elements upload

---
+++ C# Advanced/Exercises/03.Sets_and_Dictionaries_Advanced/03.02.Sets_of_Elements/02.Sets_of_Elements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._02.Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                       .Split(" ")
+                       .Select(int.Parse).ToArray();
+
+            int sizeA = sizes[0];
+            int sizeB = sizes[1];
+
+            HashSet<int> setA = FillSet(sizeA);
+            HashSet<int> setB = FillSet(sizeB);
+
+            setA.IntersectWith(setB);
+            Console.WriteLine(string.Join(" ", setA));
+        }
+
+        private static HashSet<int> FillSet(int setSize)
+        {
+            var set = new HashSet<int>();
+
+            for (int i = 0; i < setSize; i++)
+            {
+                set.Add(int.Parse(Console.ReadLine()));
+            }
+
+            return set;
+        }
+    }
+}
