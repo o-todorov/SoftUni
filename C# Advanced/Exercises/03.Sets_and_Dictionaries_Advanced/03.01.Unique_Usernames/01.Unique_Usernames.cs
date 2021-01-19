90bbaba2fd899b0bdf4392471ea5fb45265ddfe4
o-todorov <todorov.oleg44@gmail.com>
2021-01-20 00:46:24 +0200

    Unique UserNames upload

---
+++ C# Advanced/Exercises/03.Sets_and_Dictionaries_Advanced/03.01.Unique_Usernames/01.Unique_Usernames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._01.Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                set.Add(Console.ReadLine());
+            }
+
+            foreach (var str in set)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
