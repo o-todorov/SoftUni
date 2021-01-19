ddd28f3025720e10298453318263b93490b8eeef
o-todorov <todorov.oleg44@gmail.com>
2021-01-20 00:47:13 +0200

    Periodic Table upload

---
+++ C# Advanced/Exercises/03.Sets_and_Dictionaries_Advanced/03.03.Periodic_Table/03.Periodic_Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._03.Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int comproundCount = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < comproundCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in input)
+                {
+                    elements.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",elements));
+
+        }
+    }
+}
