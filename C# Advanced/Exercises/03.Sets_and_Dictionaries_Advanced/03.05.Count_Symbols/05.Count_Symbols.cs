ee425f86241fabf188684b5cc416369dc0000c5f
o-todorov <todorov.oleg44@gmail.com>
2021-01-23 11:35:13 +0200

    Count Symbols upload

---
+++ C# Advanced/Exercises/03.Sets_and_Dictionaries_Advanced/03.05.Count_Symbols/05.Count_Symbols.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._05.Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            SortedDictionary<char, int> dict = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!dict.ContainsKey(input[i]))
+                {
+                    dict[input[i]] = 1;
+                }
+                else
+                {
+                    dict[input[i]]++;
+                }
+            }
+
+            foreach (var pair in dict)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value} time/s");
+            }
+        }
+    }
+}
