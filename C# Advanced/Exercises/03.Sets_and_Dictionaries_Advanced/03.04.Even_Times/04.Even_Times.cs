4f125148f909b5c8e57ab3fff9f1c213362cae03
o-todorov <todorov.oleg44@gmail.com>
2021-01-23 11:34:55 +0200

    Even Times upload

---
+++ C# Advanced/Exercises/03.Sets_and_Dictionaries_Advanced/03.04.Even_Times/04.Even_Times.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._04.Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Dictionary<int, bool> dict = new Dictionary<int, bool>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (!dict.ContainsKey(num))
+                {
+                    dict[num] = true;
+                }
+                else
+                {
+                    dict[num] = !dict[num];
+                }
+            }
+
+            Console.WriteLine(dict.Where(i => i.Value == false).First().Key);
+        }
+    }
+}
