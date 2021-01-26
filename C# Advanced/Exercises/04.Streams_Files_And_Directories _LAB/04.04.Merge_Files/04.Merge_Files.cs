7c7392aa5db84cff1dd07b9bb955b986e6588220
o-todorov <todorov.oleg44@gmail.com>
2021-01-26 10:45:48 +0200

    04.Files And Directories Lab - Merge Files upload

---
+++ C# Advanced/Exercises/04.Streams_Files_And_Directories _LAB/04.04.Merge_Files/04.Merge_Files.cs
@@ -0,0 +1,43 @@
+﻿// Write a program that reads the contents of two text files and merges them together into a third one.
+
+using System;
+using System.IO;
+
+namespace _04._04.Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathOne = Path.Combine("data", "FileOne.txt");
+            var pathTwo = Path.Combine("data", "FileTwo.txt");
+            var output  = Path.Combine("data", "output.txt");
+
+            var textOne = File.ReadAllLines(pathOne);
+            var textTwo = File.ReadAllLines(pathTwo);
+            string[] result = new string[textOne.Length + textTwo.Length];
+
+            if (textOne.Length > textTwo.Length)
+            {
+                var t = textTwo;
+                textTwo = textOne;
+                textOne = t;
+            }
+
+            int counter = 0;
+
+            for (int i = 0; i < textOne.Length; i++)
+            {
+                result[counter++] = textOne[i];
+                result[counter++] = textTwo[i];
+            }
+
+            for (int i = textOne.Length; i < textTwo.Length; i++)
+            {
+                result[counter++] = textTwo[i];
+            }
+
+            File.WriteAllLines(output, result);
+        }
+    }
+}
