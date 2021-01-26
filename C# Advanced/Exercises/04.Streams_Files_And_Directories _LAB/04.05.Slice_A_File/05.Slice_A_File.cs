9768d22ca4bd35e7dfc7e2bece6e525b07c6f49b
o-todorov <todorov.oleg44@gmail.com>
2021-01-26 10:46:13 +0200

    04.Files And Directories Lab - Slice File upload

---
+++ C# Advanced/Exercises/04.Streams_Files_And_Directories _LAB/04.05.Slice_A_File/05.Slice_A_File.cs
@@ -0,0 +1,28 @@
+﻿// Write a program that slice text file in 4 equal parts. 
+// Name them:
+//     • Part - 1.txt, Part - 2.txt, Part - 3.txt, Part - 4.txt
+
+using System;
+using System.IO;
+
+namespace _04._05.Slice_A_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine("data", "sliceMe.txt");
+            var text = File.ReadAllText(path);
+
+            int countAll = text.Length;
+            int countSingle = countAll / 4;
+
+            for (int i = 0; i < 4; i++)
+            {
+                path = Path.Combine("data", $"Part - {i + 1}.txt");
+                File.WriteAllText(path, text.Substring(i * countSingle, countSingle));
+            }
+
+        }
+    }
+}
