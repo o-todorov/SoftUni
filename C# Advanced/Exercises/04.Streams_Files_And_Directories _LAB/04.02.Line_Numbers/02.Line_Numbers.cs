60dc64826694bc2fc58813ecd655d9e0da6425c3
o-todorov <todorov.oleg44@gmail.com>
2021-01-26 10:45:09 +0200

    04.Files And Directories Lab - Line Numbers upload

---
+++ C# Advanced/Exercises/04.Streams_Files_And_Directories _LAB/04.02.Line_Numbers/02.Line_Numbers.cs
@@ -0,0 +1,30 @@
+﻿// Write a program that reads a text file and inserts line numbers in front of each of its lines. The result should be written to another text file.
+
+using System;
+using System.IO;
+
+namespace _04._02.Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path     = Path.Combine("data", "Input.txt");
+            string ouput    = Path.Combine("data", "Output.txt");
+
+            using (TextReader reader = new StreamReader(path))
+            {
+                using (TextWriter writer = new StreamWriter(ouput))
+                {
+                    string line;
+                    int counter = 1;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{counter++}. {line}");
+                    }
+                }
+            }
+        }
+    }
+}
