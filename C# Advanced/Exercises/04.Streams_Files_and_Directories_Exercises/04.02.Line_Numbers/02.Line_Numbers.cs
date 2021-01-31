51715229f19eb871b0f40654b9c5287c19be0aec
o-todorov <todorov.oleg44@gmail.com>
2021-01-31 22:33:11 +0200

    Streams,Files and Directories - Line Numbers upload

---
+++ C# Advanced/Exercises/04.Streams_Files_and_Directories_Exercises/04.02.Line_Numbers/02.Line_Numbers.cs
@@ -0,0 +1,31 @@
+﻿// Write a program that reads a text file and inserts line numbers in front of each of its lines and count all the letters and punctuation marks. The result should be written to another text file. Use the static class File.
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _04._02.Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path     = Path.Combine("data", "text.txt");
+            string ouput    = Path.Combine("data", "Output.txt");
+
+            string[] lines  = File.ReadAllLines(path);
+            string[] result = new string[lines.Length];
+            int lineNum = 0;
+
+            foreach (var line in lines)
+            {
+                int letters = line.Where(ch => !Char.IsPunctuation(ch) && !Char.IsWhiteSpace(ch)).Count();
+                int punct   = line.Where(ch => Char.IsPunctuation(ch)).Count();
+                result[lineNum++] = $"Line {lineNum}: {line} ({letters})({punct})";
+            }
+
+            File.WriteAllLines(ouput, result);
+
+        }
+    }
+}
