e9e5ecb5eb16c9a69845c371c091c6fb7172a53b
o-todorov <todorov.oleg44@gmail.com>
2021-02-01 11:46:39 +0200

    Functional Programming - Knights Of Honor upload

---
+++ C# Advanced/Exercises/05.Functional_Programming/05.02.Knights_of__Honor/02.Knights_of__Honor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._02.Knights_of__Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Action<string> printOnNewLine = str => Console.WriteLine(str);
+                Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(str => "Sir " + str)
+                        .ToList()
+                        .ForEach(printOnNewLine);
+
+            }
+        }
+    }
+}
