56bb4e01d21fcdca9e8717a28239d22e9d3483e9
o-todorov <todorov.oleg44@gmail.com>
2021-02-01 11:45:36 +0200

    Functional Programming - Action Point upload

---
+++ C# Advanced/Exercises/05.Functional_Programming/05.01.Action_Point/01.Action_Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._01.Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printOnNewLine = str => Console.WriteLine(str);
+            Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToList()
+                    .ForEach(printOnNewLine);
+
+        }
+    }
+}
