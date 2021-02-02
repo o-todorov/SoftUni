70e8fed9d749bdba0aa50165818558d01714b5e9
o-todorov <todorov.oleg44@gmail.com>
2021-02-02 15:29:05 +0200

    Functional Programming - Custom Min Function  upload

---
+++ C# Advanced/Exercises/05.Functional_Programming/05.03.Custom_Min_Function/03.Custom_Min_Function.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._03.Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> getMinValue = (int l, int r) => l < r ? l : r;
+
+            int min = int.MaxValue;
+
+            Console.ReadLine()
+                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                       .Select(int.Parse)
+                       .ToList()
+                       .ForEach(i => min = getMinValue(i, min));
+
+                       Console.WriteLine(min);
+        }
+    }
+}
