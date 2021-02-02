a9f2bf95c677b5bc469c5a1398e802861a3e3461
o-todorov <todorov.oleg44@gmail.com>
2021-02-02 15:36:29 +0200

    Functional Programming -Reverse and Exclude upload

---
+++ C# Advanced/Exercises/05.Functional_Programming/05.06.Reverse_and_Exclude/06.Reverse_and_Exclude.cs
@@ -0,0 +1,24 @@
+﻿// Write a program that reverses a collection and removes elements that are divisible by a given integer n. Use predicates/functions.
+
+using System;
+using System.Linq;
+
+namespace _05._06.Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> indivisibleByN = (x, n) => x % n != 0;
+
+            var nums = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse);
+
+            int divider = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join(" ", nums.Where(x => indivisibleByN(x, divider)).Reverse()));
+
+        }
+    }
+}
