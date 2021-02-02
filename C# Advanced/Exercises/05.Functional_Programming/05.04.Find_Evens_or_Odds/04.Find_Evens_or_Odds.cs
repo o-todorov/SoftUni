03528f6df6c21d4272007f023e39144f8d4b90ce
o-todorov <todorov.oleg44@gmail.com>
2021-02-02 15:34:02 +0200

    Functional Programming - Find Evens and Odds upload

---
+++ C# Advanced/Exercises/05.Functional_Programming/05.04.Find_Evens_or_Odds/04.Find_Evens_or_Odds.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace _05._04.Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, bool> IsEven   = i => i % 2 == 0;
+            Func<int, bool> IsOdd    = i => i % 2 != 0;
+
+            int[] arr = Console.ReadLine()
+                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                       .Select(int.Parse)
+                       .ToArray();
+
+            Func<int, bool> isValid = (Console.ReadLine().Trim() == "even") ? IsEven : IsOdd;
+
+            List<int> nums = new List<int>();
+
+            for (int i = arr[0]; i <= arr[1]; i++)
+            {
+                nums.Add(i);
+            }
+
+            Console.WriteLine(string.Join(" ", nums.Where(isValid)));
+        }
+    }
+}
