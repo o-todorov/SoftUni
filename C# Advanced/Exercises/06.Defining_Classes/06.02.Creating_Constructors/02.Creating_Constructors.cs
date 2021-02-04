ac4351ee297ac594bf45482ea3f7c20d9043e7b9
o-todorov <todorov.oleg44@gmail.com>
2021-02-04 11:19:56 +0200

    06.Defining Classes - Creating Constructors upload

---
+++ C# Advanced/Exercises/06.Defining_Classes/06.02.Creating_Constructors/02.Creating_Constructors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person();
+            Console.WriteLine($"Person {person1.Name} is aged {person1.Age}");
+            var person2 = new Person(31);
+            Console.WriteLine($"Person {person2.Name} is aged {person2.Age}");
+            var person3 = new Person("Peter", 45);
+            Console.WriteLine($"Person {person3.Name} is aged {person3.Age}");
+
+        }
+    }
+}
