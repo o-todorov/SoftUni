2fb0e95079741679f405c487e76aa031a6542bbd
o-todorov <todorov.oleg44@gmail.com>
2021-02-04 10:38:54 +0200

    06.Defining Classes - Class Person upload

---
+++ C# Advanced/Exercises/06.Defining_Classes/06.01.Define_a_Class_Person/01.Define_a_Class_Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Peter";
+            person.Age = 29;
+            Console.WriteLine($"Person {person.Name} is aged {person.Age}");
+            var p1 = new Person("Asen", 6);
+            Console.WriteLine($"Person {p1.Name} is aged {p1.Age}");
+        }
+    }
+}
---
+++ C# Advanced/Exercises/06.Defining_Classes/06.01.Define_a_Class_Person/Person.cs
@@ -0,0 +1,27 @@
+﻿
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public Person() { }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+    }
+}
