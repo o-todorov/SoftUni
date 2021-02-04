8949065bd562d81045c5a66a765ef5f5bcd0319c
o-todorov <todorov.oleg44@gmail.com>
2021-02-04 11:22:17 +0200

    06.Defining Classes - Date Modifier upload

---
+++ C# Advanced/Exercises/06.Defining_Classes/06.05.Date_Modifier/05.Date_Modifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06._05.Date_Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+
+            int dateDayDiff = DateModifier.DateDayDiff(dateOne, dateTwo);
+            Console.WriteLine(dateDayDiff);
+        }
+    }
+}
---
+++ C# Advanced/Exercises/06.Defining_Classes/06.05.Date_Modifier/DateModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._05.Date_Modifier
+{
+    public static class DateModifier
+    {
+        public static int DateDayDiff(string dateOne,string dateTwo)
+        {
+            DateTime dateA = GetDate(dateOne);
+            DateTime dateB = GetDate(dateTwo);
+            return Math.Abs((dateA - dateB).Days);
+        }
+
+        private static DateTime GetDate(string strDate) // Format "yyyy mm dd"
+        {
+            var date = strDate.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                              .Select(int.Parse)
+                              .ToArray();
+            return new DateTime(date[0], date[1], date[2]);
+        }
+    }
+}
