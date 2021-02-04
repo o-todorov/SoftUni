92de4f5dacfc52246bf8244fce34b02fa823e352
o-todorov <todorov.oleg44@gmail.com>
2021-02-04 11:20:17 +0200

    06.Defining Classes - Oldest Family Member upload

---
+++ C# Advanced/Exercises/06.Defining_Classes/06.03.Oldest_Family_Member/03.Oldest_Family_Member.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            var family = new Family();
+            
+            for (int i = 0; i < peopleCount; i++)
+            {
+                Person person = ReadPersonFromConsole();
+                family.AddMember(person);
+            }
+
+            var oldest = family.GetOldestMember();
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+
+        }
+
+        private static Person ReadPersonFromConsole()
+        {
+            string[] person = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var name    = person[0];
+            var age     = int.Parse(person[1]);
+
+            return new Person(name, age);
+        }
+    }
+}
---
+++ C# Advanced/Exercises/06.Defining_Classes/06.03.Oldest_Family_Member/Family.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+        public Family()
+        {
+            this.people = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            people.Add(member);
+        }
+        public Person GetOldestMember()
+        {
+            return people.FirstOrDefault(people => people.Age == GetMaxAge());
+        }
+        private int GetMaxAge()
+        {
+            return people.Max(p => p.Age);
+        }
+    }
+}
