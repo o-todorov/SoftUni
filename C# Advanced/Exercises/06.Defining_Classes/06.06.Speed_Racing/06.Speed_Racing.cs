b2715c15f7db7217d8b90cdcd58ca5565ac81e2c
o-todorov <todorov.oleg44@gmail.com>
2021-02-04 11:22:35 +0200

    06.Defining Classes - Speed Racing upload

---
+++ C# Advanced/Exercises/06.Defining_Classes/06.06.Speed_Racing/06.Speed_Racing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carCount = int.Parse(Console.ReadLine());
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < carCount; i++)
+            {
+                string[] carData    = Console.ReadLine().Split();
+                Car car = carParse(carData);
+                cars.Add(car.Model, car);
+            }
+
+            string[] move;
+
+            while ((move = Console.ReadLine().Split())[0] != "End")
+            {
+                var car = cars[move[1]];
+                double km       = double.Parse(move[2]);
+
+                if (car.CanMove(km))
+                {
+                    car.Move(km);
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+            }
+
+            cars.Values.ToList().ForEach(Console.WriteLine);
+        }
+
+        private static Car carParse(string[] carData)
+        {
+            string carModel = carData[0];
+            double fuelAmount = double.Parse(carData[1]);
+            double fuelPerKM = double.Parse(carData[2]);
+
+            return new Car(carModel, fuelAmount, fuelPerKM);
+        }
+    }
+}
---
+++ C# Advanced/Exercises/06.Defining_Classes/06.06.Speed_Racing/Car.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace SpeedRacing
+{
+    class Car
+    {
+        private double travelledDistance;
+        public Car()
+        {
+            this.travelledDistance = 0;
+        }
+        public Car(string model, double fuelAmount, double fuelPerKilometer)
+            :this()
+        {
+            FuelAmount = fuelAmount;
+            Model = model;
+            FuelPerKilometer = fuelPerKilometer;
+        }
+
+        public string Model { get; set; }
+        public double FuelPerKilometer { get; set; }
+        public double FuelAmount { get; set; }
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+        public bool CanMove(double km)
+        {
+            return km <= FuelAmount / FuelPerKilometer;
+        }
+        public void Move(double km)
+        {
+            travelledDistance += km;
+            FuelAmount -= km * FuelPerKilometer;
+        }
+        public override string ToString()
+        {
+            return new StringBuilder($"{this.Model} { this.FuelAmount:f2} { this.TravelledDistance}").ToString();
+        }
+
+    }
+}
