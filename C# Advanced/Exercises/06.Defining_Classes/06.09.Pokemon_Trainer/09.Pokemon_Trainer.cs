81e3e1cc2a6b3df3a2ad59fbd6ebe1d589ad3bf3
o-todorov <todorov.oleg44@gmail.com>
2021-02-05 12:41:50 +0200

    06.Defining Classes - Pokemon Trainer upload

---
+++ C# Advanced/Exercises/06.Defining_Classes/06.09.Pokemon_Trainer/09.Pokemon_Trainer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._09.Pokemon_Trainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            var trainers = new Dictionary<string, Trainer>();
+
+            while ((input = Console.ReadLine().Split())[0] != "Tournament")
+            {
+                string  trainer     = input[0];
+                string  pokeName    = input[1];
+                string  pokElement  = input[2];
+                int     pokHealth   = int.Parse(input[3]);
+                var pokemon = new Pokemon(pokeName, pokElement, pokHealth);
+
+                if (!trainers.ContainsKey(trainer))
+                {
+                    trainers[trainer] = new Trainer(trainer);
+                }
+
+                trainers[trainer].AddPokemon(new Pokemon(pokeName, pokElement, pokHealth));
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers.Values)
+                {
+                    if( trainer.Pokemons.Any(p => p.Element == command))
+                    {
+                        trainer.BadgesCount ++;
+                    }
+                    else
+                    {
+                        trainer.Pokemons.ForEach(p => p.Health -= 10);
+                        trainer.Pokemons = trainer.Pokemons
+                                                  .Where(p => p.Health > 0)
+                                                  .ToList();
+                    }
+                }
+            }
+
+            Action<Trainer> printTrainer = tr => Console.WriteLine($"{tr.Name} {tr.BadgesCount} {tr.Pokemons.Count}"); 
+
+            trainers.Values
+                    .OrderByDescending(t => t.BadgesCount)
+                    .ToList()
+                    .ForEach(printTrainer);
+        }
+    }
+}
---
+++ C# Advanced/Exercises/06.Defining_Classes/06.09.Pokemon_Trainer/Trainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._09.Pokemon_Trainer
+{
+    class Trainer
+    {
+        private List<Pokemon> pokemons;
+
+        public Trainer()
+        {
+            this.pokemons = new List<Pokemon>();
+            BadgesCount = 0;
+            Name = "Unknown";
+        }
+        public Trainer(string name):this()
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public int BadgesCount { get; set; }
+        public List<Pokemon> Pokemons 
+        { 
+            get => pokemons; 
+            set => pokemons = value; 
+        }
+        public void AddPokemon(Pokemon pokemon)
+        {
+            pokemons.Add(pokemon);
+        }
+    }
+}
