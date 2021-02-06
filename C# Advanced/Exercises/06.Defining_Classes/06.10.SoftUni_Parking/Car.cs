714a6993c5965699e99b7668e6b8faaac84840a0
o-todorov <todorov.oleg44@gmail.com>
2021-02-06 00:44:06 +0000

    06.Defining Classes - SoftUni parking upload

---
+++ C# Advanced/Exercises/06.Defining_Classes/06.10.SoftUni_Parking/Car.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+        public Car() { }
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public int HorsePower { get => horsePower; set => horsePower = value; }
+        public string RegistrationNumber { get => registrationNumber; set => registrationNumber = value; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Make: {make}");
+            sb.AppendLine($"Model: {model}");
+            sb.AppendLine($"HorsePower: {horsePower}");
+            sb.Append($"RegistrationNumber: {registrationNumber}");
+            return sb.ToString();
+        }
+    }
+}
+
---
+++ C# Advanced/Exercises/06.Defining_Classes/06.10.SoftUni_Parking/Parking.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private Dictionary<string, Car> cars;
+        private int capacity;
+        public Parking()
+        {
+            this.cars = new Dictionary<string, Car>();
+        }
+        public Parking(int capacity) : this()
+        {
+            this.capacity = capacity;
+        }
+
+        public Dictionary<string, Car> MyProperty { get => cars; set => cars = value; }
+        public int Count { get => cars.Count; }
+        public string AddCar(Car car)
+        {
+            if (cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else if (cars.Count == capacity)
+            {
+                return "Parking is full!";
+            }
+            else
+            {
+                cars.Add(car.RegistrationNumber, car);
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+
+        }
+        public string RemoveCar(string registrationNumber)
+        {
+            if (cars.ContainsKey(registrationNumber))
+            {
+                cars.Remove(registrationNumber);
+                return $"Successfully removed {registrationNumber}";
+            }
+            else
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+        }
+        public Car GetCar(string registrationNumber)
+        {
+            if (cars.ContainsKey(registrationNumber))
+            {
+                return cars[registrationNumber];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            registrationNumbers.ForEach(num =>
+            {
+                if (cars.ContainsKey(num)) cars.Remove(num);
+            });
+        }
+    }
+}
