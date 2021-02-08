c16d7b22ec8491476f7768d14e42c7538d32f1d7
o-todorov <todorov.oleg44@gmail.com>
2021-02-08 15:54:11 +0000

    Generics - Box Of T

---
+++ C# Advanced/Exercises/08.Generics/01.BoxOfT/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> items;
+        public Box()
+        {
+            this.items = new List<T>();
+        }
+        public int Count { get => items.Count; }
+        public void Add(T element)
+        {
+            items.Add(element);
+        }
+        public T Remove() // Removes top(last) element
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Can't remove from empty box");
+            }
+
+            var retVal = items[Count - 1];
+            items.RemoveAt(Count - 1);
+
+            return retVal;
+        }
+    }
+}
