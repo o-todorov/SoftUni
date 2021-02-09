054edeb951d1ad3e1946d3f8db2fbc33d22306a7
o-todorov <todorov.oleg44@gmail.com>
2021-02-09 14:22:14 +0000

    Generics - Threeuple Class upload

---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.08.Threeuple/08.Threeuple.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Threeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*{first name} {last name} {address} {town}
+            • The second line is holding a name, beer liters, and a boolean variable with value - drunk or not. Format:
+            {name} {liters of beer} {drunk or not}
+            • The last line will hold a name, a bank balance (double) and a bank name. Format:
+            {name} {account balance} {bank name}*/
+            // {firstElement} -> {secondElement} -> {thirdElement}
+
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var fullName = $"{input[0]} {input[1]}";
+            var threupleOne = new Threeuple<string, string, string>(fullName,input[2],input[3]);
+
+            input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            bool drunk = input[2] == "drunk" ? true : false;
+            var threupleTwo = new Threeuple<string, int, bool>(input[0], int.Parse(input[1]), drunk);
+
+            input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var threupleThree = new Threeuple<string, double, string>(input[0], double.Parse(input[1]), input[2]);
+
+            PrintThreuple(threupleOne);
+            PrintThreuple(threupleTwo);
+            PrintThreuple(threupleThree);
+        }
+        public static void PrintThreuple<T1, T2, T3>(Threeuple<T1, T2, T3> threuple)
+        {
+            Console.WriteLine(string.Join(" -> ", threuple.ToStringArray()));
+        }
+    }
+}
---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.08.Threeuple/ThreeupleClass.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public Threeuple()
+        {
+        }
+        public Threeuple(T1 item1, T2 item2, T3 item3)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+
+        public T1 item1 { get; set; }
+        public T2 item2 { get; set; }
+        public T3 item3 { get; set; }
+        public string[] ToStringArray()
+        {
+            string[]retArr= new string[3];
+            retArr[0] = item1.ToString();
+            retArr[1] = item2.ToString();
+            retArr[2] = item3.ToString();
+
+            return retArr;
+        }
+    }
+}
