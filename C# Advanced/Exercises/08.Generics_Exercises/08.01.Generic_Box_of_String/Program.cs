aa66f24c76e1d0a98ac372332b54c9b02dc435e0
o-todorov <todorov.oleg44@gmail.com>
2021-02-09 14:19:46 +0000

    Generics - Box Of Integers upload

---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.01.Generic_Box_of_String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08._01.Generic_Box_of_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine().Trim());
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
