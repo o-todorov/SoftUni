25a0d48ac3a41d0688ca0629d75a22db728e7f65
o-todorov <todorov.oleg44@gmail.com>
2021-02-09 21:04:00 +0000

    Generic_Count_Method_Strings

---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.05.Generic_Count_Method_Strings/05.CountMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_Count_Method_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string[] arr = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                arr[i]=Console.ReadLine();
+            }
+
+            // Next 2 lines are to be changed for another type
+            var comparer = new Box<string>(Console.ReadLine());
+            var list     = arr.Select(str => new Box<string>(str)).ToList();
+
+            Console.WriteLine(GetCount(list, comparer));
+        }
+
+        public static int GetCount<T>(List<T> list, T comp)
+            where T : IComparable
+        {
+            return list.Where(box => box.CompareTo(comp) > 0).Count();
+        }
+    }
+}
