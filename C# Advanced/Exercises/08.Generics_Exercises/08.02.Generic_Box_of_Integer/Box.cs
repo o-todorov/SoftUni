df1be7c4fe355f7caec8ca9a333e470813b3e973
o-todorov <todorov.oleg44@gmail.com>
2021-02-09 14:21:06 +0000

    Generics - Box Of Integers upload

---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.02.Generic_Box_of_Integer/Box.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace _08._01.Generic_Box_of_String
+{
+    public class Box<T>
+    {
+        private readonly T item;
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(item.GetType().FullName.ToString()).Append(": ");
+            sb.Append(item.ToString());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.02.Generic_Box_of_Integer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08._01.Generic_Box_of_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Box<int> box = new Box<int>(int.Parse(Console.ReadLine().Trim()));
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
