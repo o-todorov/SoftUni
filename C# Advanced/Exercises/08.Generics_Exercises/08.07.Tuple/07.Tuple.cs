b1d04bcc09b0342817367eb3734594ffe3050177
o-todorov <todorov.oleg44@gmail.com>
2021-02-09 14:21:37 +0000

    Generics - Tuple upload

---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.07.Tuple/07.Tuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TupleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            var tuple = new MyTuple<string, string>($"{input[0]} {input[1]}", input[2]);
+            Console.WriteLine($"{tuple.item1} -> {tuple.item2}");
+
+            input = Console.ReadLine().Split();
+            var tupleOne = new MyTuple<string, int>(input[0], int.Parse(input[1]));
+            Console.WriteLine($"{tupleOne.item1} -> {tupleOne.item2}");
+
+            input = Console.ReadLine().Split();
+            var tupleTwo = new MyTuple<int, double>(int.Parse(input[0]), double.Parse(input[1]));
+            Console.WriteLine($"{tupleTwo.item1} -> {tupleTwo.item2}");
+        }
+    }
+}
---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.07.Tuple/Tuple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TupleTest
+{
+    class MyTuple<T, V>
+    {
+        public MyTuple(T item1, V item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public T item1 { get; }
+        public V item2 { get; }
+    }
+}
