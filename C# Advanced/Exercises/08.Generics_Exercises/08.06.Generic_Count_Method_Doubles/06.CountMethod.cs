7e9f1e0b34d7f82fc3225e3286f234433f78f1bc
o-todorov <todorov.oleg44@gmail.com>
2021-02-09 21:04:22 +0000

    Generic_Count_Method_Doubles

---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.06.Generic_Count_Method_Doubles/06.CountMethod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_Count_Method_Doubles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string[] arr = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+
+                    // Next 2 lines are to be changed for another type
+            var comparer    = CreateBox(double.Parse(Console.ReadLine()));
+            var list        = CreateListOfBox(arr.Select(double.Parse).ToArray());
+
+                    // Next 2 lines are for for string type instead
+            //var comparer    = CreateBox(Console.ReadLine());
+            //var list        = CreateListOfBox(arr);
+
+            Console.WriteLine(GetCount(list, comparer));
+        }
+
+        private static List<Box<T>> CreateListOfBox<T>(T[] arr )
+            where T : IComparable
+        {
+            return new List<Box<T>>(arr.Select(v => new Box<T>(v)));
+        }
+
+        private static Box<T> CreateBox<T>(T v)
+            where T:IComparable
+        {
+            return new Box<T>(v);
+        }
+
+        public static int GetCount<T>(List<T> list, T comp)
+            where T : IComparable
+        {
+            return list.Where(box => box.CompareTo(comp) > 0).Count();
+        }
+    }
+}
---
+++ C# Advanced/Exercises/08.Generics_Exercises/08.06.Generic_Count_Method_Doubles/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Generic_Count_Method_Doubles
+{
+    public class Box<T> : IComparable
+        where T : IComparable
+    {
+        private readonly T item;
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return item.CompareTo((obj as Box<T>).item);
+        }
+    }
+}
