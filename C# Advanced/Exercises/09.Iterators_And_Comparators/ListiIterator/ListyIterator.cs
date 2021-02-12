a9e26c5f43ea356072733032f7503e9de54f5ef3
o-todorov <todorov.oleg44@gmail.com>
2021-02-12 13:33:13 +0000

    09.Iterators And Comparators Exercise - ListyIterator

---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/ListiIterator/ListyIterator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListiIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> items;
+        private int currentIndex;
+        public ListyIterator(params T[] collection)
+        {
+            items = collection.ToList();
+            currentIndex = items.Count > 0 ? 0 : -1;
+        }
+        public bool Move()
+        {
+            if (HasNext)
+            {
+                currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+        public bool HasNext => currentIndex + 1 < items.Count;
+        public void Print()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(items[currentIndex].ToString());
+        }
+    }
+}
---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/ListiIterator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ListiIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var command = input.Split().Skip(1).ToArray();
+            var listIterator = new ListyIterator<string>(command);
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(listIterator.Move());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            listIterator.Print();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listIterator.HasNext);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
