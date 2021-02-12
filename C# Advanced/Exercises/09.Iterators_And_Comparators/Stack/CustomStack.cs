e1b1b82e016fecb7553f5f2e62de55292854e1f9
o-todorov <todorov.oleg44@gmail.com>
2021-02-12 13:33:41 +0000

    09.Iterators And Comparators Exercise - Custom Stack

---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/Stack/CustomStack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class CustomStack<T>: IEnumerable<T>
+    {
+        private List<T> items;
+        public CustomStack()
+        {
+            items = new List<T>();
+        }
+        public int Count { get => items.Count;  }
+        public void Push(T value)
+        {
+            items.Add(value);
+        }
+        public T Pop()
+        {
+            T ret = items[Count - 1];
+            items.RemoveAt(Count - 1);
+            return ret;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/Stack/CustomStackSturtUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    class CustomStackSturtUp
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] input;
+                var stack = new CustomStack<int>();
+
+                while ((input = Console.ReadLine().Split(" ",2))[0] != "END")
+                {
+                    switch (input[0])
+                    {
+                        case "Push":
+                            input[1].Split(", ")    
+                                    .Select(int.Parse)
+                                    .ToList()
+                                    .ForEach(stack.Push);
+                            break;
+                        case "Pop":
+                            try
+                            {
+                                stack.Pop();
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("No elements");
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Unknown command!");
+                            break;
+                    }
+                }
+
+                foreach (var item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+
+                foreach (var item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
