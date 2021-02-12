5240bb83e226ca1bba34e267830652779cd1bac6
o-todorov <todorov.oleg44@gmail.com>
2021-02-12 13:32:18 +0000

    09.Iterators And Comparators Exercise - Equality Logic

---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/EqualityLogic/EqualityLogicStartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    class EqualityLogicStartUp
+    {
+        static void Main(string[] args)
+        {
+            var sortedSetPerson = new SortedSet<Person>();
+            var hashSetPerson = new HashSet<Person>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                sortedSetPerson.Add(new Person(input[0], int.Parse(input[1])));
+                hashSetPerson.Add(new Person(input[0], int.Parse(input[1])));
+            }
+
+            Console.WriteLine(sortedSetPerson.Count);
+            Console.WriteLine(hashSetPerson.Count);
+        }
+    }
+}
---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/EqualityLogic/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EqualityLogic
+{
+    class Person: IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name    = name;
+            Age     = age;
+        }
+
+        public string  Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = (result = Name.CompareTo(other.Name)) != 0 ? result :
+                             Age.CompareTo(other.Age);
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person person &&
+                   Name == person.Name &&
+                   Age == person.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 0;
+
+            foreach (var ch in Name)
+            {
+                hashCode += ch * Age;
+            }
+
+            return hashCode % 1000;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age}";
+        }
+    }
+}
