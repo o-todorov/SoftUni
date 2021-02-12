bf167108fb464c24c97eff6e5e77c3d1fb9728af
o-todorov <todorov.oleg44@gmail.com>
2021-02-12 13:31:54 +0000

    09.Iterators And Comparators Exercise - Comparing Objects

---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/ComparingObjects/ComparingObjectsStartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    class ComparingObjectsStartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] arr;
+            List<Person> people = new List<Person>();
+
+            while ((arr = Console.ReadLine().Split())[0] != "END")
+            {
+                people.Add(new Person(arr[0], int.Parse(arr[1]), arr[2]));
+            }
+
+            int index = int.Parse(Console.ReadLine());
+            var personToCompare = people[index - 1];
+            var eq = people.Where(p => p.CompareTo(personToCompare) == 0);
+            int equalTo = eq.Count();
+            int notEqualTo = people.Count - equalTo;
+
+            if (equalTo <= 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{ equalTo} { notEqualTo} {people.Count}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/ComparingObjects/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ComparingObjects
+{
+    class Person: IComparable
+    {
+        public Person(string name, int age, string town)
+        {
+            Name    = name;
+            Age     = age;
+            Town    = town;
+        }
+
+        public string  Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+
+        public int CompareTo(object obj)
+        {
+            Person other = obj as Person;
+            int result;
+            return (result = Name.CompareTo(other.Name)) != 0 ? result :
+                   (result = Age.CompareTo(other.Age)) != 0 ? result :
+                             Town.CompareTo(other.Town);
+        }
+        //public override string ToString()
+        //{
+        //    return $"{Name} - {Age} - {Town}";
+        //}
+    }
+}
