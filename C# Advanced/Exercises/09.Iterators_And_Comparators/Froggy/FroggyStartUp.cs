ea1fe7a7cce1fad64bcf3bc996998a82f87f8d01
o-todorov <todorov.oleg44@gmail.com>
2021-02-12 13:32:31 +0000

    09.Iterators And Comparators Exercise - Frogi

---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/Froggy/FroggyStartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    class FroggyStartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                       .Split(", ")
+                       .Select(int.Parse)
+                       .ToArray();
+
+            var lake = new Lake(arr);
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/Froggy/Lake.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Froggy
+{
+    class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+
+        public Lake(params int[] stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            int idx = 0;
+
+            for (idx = 0; idx < stones.Length; idx += 2)
+            {
+                yield return stones[idx];
+            }
+
+            idx = --idx < stones.Length ? idx : idx - 2;
+
+            for (; idx >= 1; idx -= 2)
+            {
+                yield return stones[idx];
+            }
+        }
+    }
+}
