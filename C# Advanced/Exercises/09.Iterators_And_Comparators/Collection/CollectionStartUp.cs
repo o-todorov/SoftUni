6e2ca6029142f359bb05c8703a41bda1bcf348b0
o-todorov <todorov.oleg44@gmail.com>
2021-02-12 13:31:32 +0000

    09.Iterators And Comparators Exercise - Collection

---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/Collection/CollectionStartUp.cs
@@ -0,0 +1,49 @@
+﻿using ListiIterator;
+using System;
+using System.Linq;
+
+namespace Collection
+{
+    class CollectionStartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var command = input.Split().Skip(1).ToArray();
+            var listIterator = new ListyIterator<string>(command);
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(listIterator.Move());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            listIterator.Print();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listIterator.HasNext);
+                        break;
+                    case "PrintAll":
+                        foreach (var item in listIterator)
+                        {
+                            Console.Write($"{item} ");
+                        }
+
+                        Console.WriteLine();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exercises/09.Iterators_And_Comparators/Collection/ListyIterator.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListiIterator
+{
+    public class ListyIterator<T> : IEnumerable
+    {
+        private List<T> items;
+        private int currentIndex;
+        public ListyIterator(params T[] collection)
+        {
+            items = collection.ToList();
+            currentIndex = items.Count > 0 ? 0 : -1;
+        }
+        public bool Move()
+        {
+            if (HasNext)
+            {
+                currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+        public bool HasNext => currentIndex + 1 < items.Count;
+        public void Print()
+        {
+            if (currentIndex == -1)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(items[currentIndex].ToString());
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                yield return items[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
