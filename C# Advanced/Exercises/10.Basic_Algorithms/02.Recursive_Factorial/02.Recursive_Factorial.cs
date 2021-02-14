86ed2b941ce7c64254cfdb0465433cca380578f2
o-todorov <todorov.oleg44@gmail.com>
2021-02-14 21:13:31 +0000

    Recursive Factorial

---
+++ C# Advanced/Exercises/10.Basic_Algorithms/02.Recursive_Factorial/02.Recursive_Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02.Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            long fact = Recursive_Factorial(num);
+            Console.WriteLine(fact);
+        }
+
+        private static long Recursive_Factorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+
+            return num * Recursive_Factorial(num - 1);
+        }
+    }
+}
