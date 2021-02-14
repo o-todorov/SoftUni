51c19af1c2452e1900a184512b672a559292061e
o-todorov <todorov.oleg44@gmail.com>
2021-02-14 21:13:12 +0000

    Recursive array sum

---
+++ C# Advanced/Exercises/10.Basic_Algorithms/01.RecursiveArraySum/.01.RecursiveArraySum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                       .Split(" ")
+                       .Select(int.Parse)
+                       .ToArray();
+            int sum = CalculateSum(arr, 0);
+            Console.WriteLine(sum);
+        }
+
+        private static int CalculateSum(int[] arr, int currIndex)
+        {
+            if (currIndex >= arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[currIndex] + CalculateSum(arr, currIndex + 1);
+        }
+    }
+}
