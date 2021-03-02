83e94458def3c4c306009a65072d186b0abcfb39
o-todorov <todorov.oleg44@gmail.com>
2021-03-02 23:03:56 +0000

    Inheritance - Person - upload

---
+++ C# OOP/Exercises/Person/Person.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age  = age;
+        }
+
+        public string Name { get; set; }
+        public virtual int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+
+
+}
