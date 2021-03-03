199d43fb926eda9e79e3eb95815c49099ba177bb
o-todorov <todorov.oleg44@gmail.com>
2021-03-03 00:08:53 +0000

    Inheritance Restorant upload

---
+++ C# OOP/Exercises/Restaurant/Coffee.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Restaurant
+{
+    public class Coffee:HotBeverage
+    {
+        private const decimal coffeePrice       = 3.50M;
+        private const double  coffeeMilliliters = 50.0;
+        public Coffee(string name, double caffeine)
+            : base(name, coffeePrice, coffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+        public double CoffeeMilliliters { get => base.Milliliters;}
+        public decimal CoffeePrice { get => base.Price; }
+        public double Caffeine { get; set; }
+    }
+}
---
+++ C# OOP/Exercises/Restaurant/MainDish.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Restaurant
+{
+    public class MainDish:Food
+    {
+        public MainDish(string name, decimal price, double grams)
+            :base(name, price, grams)
+        {
+
+        }
+    }
+}
