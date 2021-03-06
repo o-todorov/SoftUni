306a50d10dd986c7cccec881587bed8766df80c6
o-todorov <todorov.oleg44@gmail.com>
2021-03-06 21:17:43 +0000

    Need For Speed upload

---
+++ C# OOP/Exercises/Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RaceMotorcycle car = new RaceMotorcycle(200, 30);
+            car.Drive(10);
+            System.Console.WriteLine(car.Fuel);
+        }
+    }
+}
