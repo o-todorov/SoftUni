01a504b34b72fe2e8b1d433a71bf27fa8e5c2d8c
o-todorov <todorov.oleg44@gmail.com>
2021-03-07 20:15:44 +0000

    Shopping Spree upload

---
+++ C# OOP/Exercises/02.Encapsulation/02.03_Shopping_Spree/03_Shopping_Spree.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._03_Shopping_Spree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Person> people;
+            Dictionary<string, Product> products;
+
+            try
+            {
+                people = ReadFromConsole()
+                                .Select(s => new Person(s[0].Trim(), decimal.Parse(s[1].Trim())))
+                                .ToDictionary(p => p.Name, p => p);
+
+                products = ReadFromConsole()
+                                .Select(s => new Product(s[0].Trim(), decimal.Parse(s[1].Trim())))
+                                .ToDictionary(p => p.Name, p => p);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            string[] input;
+
+            while ((input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries))[0] != "END")
+            {
+                var person  = people[input[0]];
+                var product = products[input[1]];
+
+                if (person.Money >= product.Cost)
+                {
+                    person.Money -= product.Cost;
+                    person.AddProduct(product);
+                    Console.WriteLine($"{person} bought {product}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person} can't afford {product}");
+                }
+            }
+
+            PrintPeople(people);
+        }
+
+        private static void PrintPeople(Dictionary<string, Person> people)
+        {
+            foreach (var person in people.Values)
+            {
+                if (person.BagCount == 0)
+                {
+                    Console.WriteLine($"{person} - Nothing bought");
+                }
+                else
+                {
+                    Console.WriteLine($"{person} - {string.Join(", ", person.Bag)}");
+                }
+            }
+        }
+
+        private static string[][] ReadFromConsole()
+        {
+            return Console.ReadLine()
+                           .Trim()
+                           .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                           .Select(s => s.Trim().Split('='))
+                           .ToArray();
+        }
+    }
+}
---
+++ C# OOP/Exercises/02.Encapsulation/02.03_Shopping_Spree/Person.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace _02._03_Shopping_Spree
+{
+    class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            this.Name  = name;
+            this.Money = money;
+            bag = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => name;
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => money;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+        public int BagCount => bag.Count;
+        public IReadOnlyCollection<Product> Bag => bag.AsReadOnly();
+        public void AddProduct(Product product)
+        {
+            bag.Add(product);
+        }
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+    }
+}
