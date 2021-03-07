5ae414e18a125ef3c3fbca83e205c9b418b7fc59
o-todorov <todorov.oleg44@gmail.com>
2021-03-07 12:33:05 +0000

    Restorant upload

---
+++ C# OOP/Exercises/Inheritance/Restaurant/Cake.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Restaurant
+{
+    public class Cake:Dessert
+    {
+        private const double  defaultCalories   = 1000.0;
+        private const double  defaultGrams      = 250.0;
+        private const decimal defaultCakePrice  = 5M;
+        public Cake(string name)
+            : base(name, defaultCakePrice, defaultGrams, defaultCalories)
+        {
+
+        }
+    }
+}
