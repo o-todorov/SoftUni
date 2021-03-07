3d082e0e05b23307e0e5bb3c68834e33a2afb1c3
o-todorov <todorov.oleg44@gmail.com>
2021-03-07 12:34:14 +0000

    Class Box upload

---
+++ C# OOP/Exercises/02.Encapsulation/02.01.Class_Box_Data/Box.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace ClassBox
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double heihght)
+        {
+            Length  = length;
+            Width   = width;
+            Height = heihght;
+        }
+        private double Length 
+        {
+            set 
+            {
+                ValidateProp(value, "Length");
+                length = value; 
+            } 
+        }
+        private double Width
+        {
+            set
+            {
+                ValidateProp(value, "Width");
+                width = value;
+            }
+        }
+        private double Height
+        {
+            set
+            {
+                ValidateProp(value, "Height");
+                height = value;
+            }
+        }
+        private void ValidateProp(double value, string side)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{side} cannot be zero or negative.");
+            }
+        }
+        public double SurfaceArea => LateralSurfaceArea + length * width * 2;
+        public double LateralSurfaceArea => (length + width) * height * 2;
+        public double Volume => length * width * height;
+    }
+}
---
+++ C# OOP/Exercises/02.Encapsulation/02.01.Class_Box_Data/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassBox
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box box;
+
+            try
+            {
+                box = new Box(GetDim(), GetDim(), GetDim());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea:f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea:f2}");
+            Console.WriteLine($"Volume - {box.Volume:f2}");
+        }
+
+        private static double GetDim()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
