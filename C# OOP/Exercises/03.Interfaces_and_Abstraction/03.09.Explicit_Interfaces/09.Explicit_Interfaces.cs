ebe6d75f838d030d5e87cc2cdd9eab26c0a2ac29
o-todorov <todorov.oleg44@gmail.com>
2021-03-14 07:19:46 +0000

    Explicit Interfaces upload

---
+++ C# OOP/Exercises/03.Interfaces_and_Abstraction/03.09.Explicit_Interfaces/09.Explicit_Interfaces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+
+            while ((input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries))[0] != "End")
+            {
+                var citizen = new Citizen(input[0], input[1], int.Parse(input[2]));
+                Console.WriteLine(citizen.Name);
+                Console.WriteLine((citizen as IResident).GetName());
+            }
+
+        }
+    }
+}
---
+++ C# OOP/Exercises/03.Interfaces_and_Abstraction/03.09.Explicit_Interfaces/Citizen.cs
@@ -0,0 +1,27 @@
+﻿namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, int age)
+        {
+            Name    = name;
+            Country = country;
+            Age     = age;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Country { get; private set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {Name}";
+        }
+    }
+}
