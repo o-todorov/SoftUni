18208831a5a23ce121663182fdbf72b236b09835
o-todorov <todorov.oleg44@gmail.com>
2021-03-27 18:01:32 +0000

    Enter Numbers initial upload

---
+++ C# OOP/Exercises/05.ExceptionHandling/02.EnterNumbers/02.EnterNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02.EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start   = ConsoleReadRange("Enter range start number:");
+            int end     = ConsoleReadRange("Enter range end number:");
+
+            int counter = 0;
+
+            while (counter < 10)
+            {
+                try
+                {
+                    ReadNumber(start, end);
+                    counter++;
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+        }
+
+        private static int ConsoleReadRange(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void ReadNumber(int start, int end)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (OutOfRange(num, start, end)) throw new InvalidOperationException($"Input number {num} out of bounds");
+
+        }
+
+        private static bool OutOfRange(int num, int start, int end)
+        {
+            return num < start || num > end;
+        }
+    }
+}
