a3909610b8d2fbdb29d12871727168ecfa0b4408
o-todorov <todorov.oleg44@gmail.com>
2021-03-27 18:02:54 +0000

    Fixing Vol.2 initial upload

---
+++ C# OOP/Exercises/05.ExceptionHandling/05.04.Fixing2/04.Fixing2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._04.Fixing2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+            }
+            catch (OverflowException ofe)
+            {
+                Console.WriteLine(ofe.Message);
+                return;
+            }
+
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            Console.ReadLine();
+        }
+    }
+}
