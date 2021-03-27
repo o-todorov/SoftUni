8fa36553ca22fe97f69723b5c195597549f07528
o-todorov <todorov.oleg44@gmail.com>
2021-03-27 18:02:03 +0000

    Square Roots initial upload

---
+++ C# OOP/Exercises/05.ExceptionHandling/05.01.SquareRoot/01.SquareRoot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._01.SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num < 0) throw new InvalidOperationException("The number must be pozitive");
+                Console.WriteLine(Math.Sqrt(num));
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
