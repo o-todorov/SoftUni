32f090bf752ea7a344656b886e1a1577c3f31cbf
o-todorov <todorov.oleg44@gmail.com>
2021-03-31 18:19:12 +0100

    Valid Person Initial upload

---
+++ C# OOP/Exercises/05.ExceptionHandling/05.06.ValidPerson/06.ValidPerson.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05._06.ValidPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] persons = new string[][]
+            {
+                new string[]{"Petyr","Petrov","26" },
+                new string[]{"Petyr","Petrov","-1" },
+                new string[]{"Petyr","Petrov","121" },
+                new string[]{"","Petrov","26" },
+                new string[]{"Petyr","","26" },
+                new string[]{null,"Petrov","26" },
+                new string[]{"Petyr",null,"26" },
+                new string[]{"Petyr","Petrov","26" }
+            };
+
+            foreach (var p in persons)
+            {
+                try
+                {
+                    Person person = new Person(p[0], p[1] as string, int.Parse(p[2]));
+                    Console.WriteLine($"Person - {person.FirstName} {person.LastName} age: {person.Age}");
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine(ane.Message);
+                }
+                catch (ArgumentOutOfRangeException aoore)
+                {
+                    Console.WriteLine(aoore.Message);
+                }
+            }
+        }
+    }
+}
---
+++ C# OOP/Exercises/05.ExceptionHandling/05.06.ValidPerson/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _05._06.ValidPerson
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName { get => firstName; set => firstName = ValidateName(value, "First Name"); }
+
+        public string LastName { get => lastName; set => lastName = ValidateName(value, "Last Name"); }
+        public int Age
+        {
+            get => age;
+
+            set
+            {
+                if (value >= 0 && value <= 120)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(
+                            "value",
+                            "Age should be in range [0 - 120].");
+                }
+            }
+        }
+
+        private string ValidateName(string value, string argName)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(
+                    "value",
+                    $"The {argName} cannot be null or empty.");
+            }
+
+            return value;
+        }
+
+    }
+}
