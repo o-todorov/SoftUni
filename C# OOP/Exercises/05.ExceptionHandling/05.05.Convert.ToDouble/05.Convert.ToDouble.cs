8cea558e385e878ba672d4732786cbc4982af3b9
o-todorov <todorov.oleg44@gmail.com>
2021-03-31 18:18:45 +0100

    Convert to double initial upload

---
+++ C# OOP/Exercises/05.ExceptionHandling/05.05.Convert.ToDouble/05.Convert.ToDouble.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._05.ConvertToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var obj = Console.ReadLine();
+
+            try
+            {
+                double dbl = Convert.ToDouble(obj);
+                Console.WriteLine(dbl.ToString());
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            catch (OverflowException ofe)
+            {
+                Console.WriteLine(ofe.Message);
+            }
+            catch (InvalidCastException ice)
+            {
+                Console.WriteLine(ice.Message);
+            }
+
+        }
+    }
+}
