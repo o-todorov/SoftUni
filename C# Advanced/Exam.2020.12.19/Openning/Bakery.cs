97403829290cbf2ae8e7c7c2343b96b708bad7d9
o-todorov <todorov.oleg44@gmail.com>
2021-04-11 18:36:35 +0100

    Exam Bakery Openning initial upload

---
+++ C# Advanced/Exam.2020.12.19/Openning/Bakery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+
+        public Bakery(string name, int capacity)
+        {
+            Name        = name;
+            Capacity    = capacity;
+            data        = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; private set; }
+        public int Count => data.Count;
+        public void Add(Employee employee)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(employee);
+            }
+        }
+        public bool Remove(string name)
+        {
+            return data.RemoveAll(e => e.Name == name) > 0;
+        }
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(e => e.Age)
+                       .FirstOrDefault();
+        }
+        public Employee GetEmployee(string name)
+        {
+            return data.FirstOrDefault(e => e.Name == name);
+        }
+        public string Report()
+        {
+            return $"Employees working at Bakery {Name}:" +
+                    Environment.NewLine +
+                    string.Join(Environment.NewLine, data);
+        }
+    }
+}
