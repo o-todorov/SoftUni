dd75e6b3d2b2ff36c2da1b2b83bec0f01db46959
o-todorov <todorov.oleg44@gmail.com>
2021-04-14 22:07:03 +0100

    Exam Exam.2020.10.25 Scheduling initial upload

---
+++ C# Advanced/Exam.2020.10.25/Scheduling/Scheduling.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    class Scheduling
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<int> tasks = new Stack<int>(Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse));
+            Queue<int> threads = new Queue<int>(Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse));
+
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                var task = tasks.Peek();
+                var thread = threads.Peek();
+
+                if (task == taskToKill)
+                {
+                    Console.WriteLine($"Thread with value {thread} killed task {taskToKill}");
+                    break;
+                }
+
+                if (thread >= task)
+                {
+                    tasks.Pop();
+                }
+
+                threads.Dequeue();
+
+            }
+
+            Console.WriteLine(string.Join(" ",threads));
+        }
+    }
+}
