5f8c5ee96f096d705978816ae16fada3e25a46f1
o-todorov <todorov.oleg44@gmail.com>
2021-06-14 02:04:21 +0100

    DOM Labs Initial upload

---
+++ JS Advanced/Exercises/04.DOM-Introduction-Lab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let linkElement = document.querySelector('#more');
+    let txtElement = document.querySelector('#text');
+    txtElement.setAttribute('style', 'display:inline');
+    linkElement.setAttribute('style', 'display:none');
+}
