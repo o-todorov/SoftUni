95cdd330a90ccf1c50aced1ff78f0f9aca6c3cdb
o-todorov <todorov.oleg44@gmail.com>
2021-06-19 15:36:22 +0100

    Additional uploads

--- JS Advanced/Exercises/04.DOM-Introduction-Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/Exercises/04.DOM-Introduction-Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,7 +1,9 @@
 function extract(content) {
-    const regex = /(?<=\()[^\(\)]*(?=\))/g;
-    return  document.getElementById(content)
+    const regex = /\([^\)]*\)/g;
+
+    return document.getElementById(content)
                     .textContent
                     .match(regex)
+                    .map(x => x.slice(1, -1))
                     .join('; ');
 }
