9694d07955994f637dd18c1ef08388329dbfd1e8
o-todorov <todorov.oleg44@gmail.com>
2021-06-21 22:18:37 +0100

    DOM Events - Sections initial upload

---
+++ JS Advanced/Exercises/05.DOM-Events/01. Sections/script.js
@@ -0,0 +1,24 @@
+function create(words) {
+   let container = document.querySelector('#content');
+   container.addEventListener('click', onclick);
+
+   words.forEach(word => {
+      let para = document.createElement('p');
+      para.textContent = word;
+      para.style.display = 'none';
+      let div = document.createElement('div');
+      div.appendChild(para);
+      container.appendChild(div);
+   });
+
+
+   function onclick(event){
+      if(event.target.matches('#content div')){
+         event.target.firstChild.style.display = 'block';
+      content
+      /*if(event.target.parentElement.tagName = 'content'){
+         event.target.firstChild.style.display = 'block';
+      content*/
+      }
+   }
+}
