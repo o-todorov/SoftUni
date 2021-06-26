ce2de70fa9dec5e0d115e9e839624ca7b23af695
o-todorov <todorov.oleg44@gmail.com>
2021-06-26 21:15:16 +0100

    Parking initial 88% 1 test fails

---
+++ JS Advanced/Exercises/Exams/Exam.2021.02.12/02.Parking/Parking.js
@@ -0,0 +1,70 @@
+class Parking{
+
+    constructor(capacity){
+        this.capacity = Number(capacity);
+        this.vehicles = [];
+    }
+    addCar(carModel, carNumber){
+        if(this.vehicles.length >= this.capacity){
+            throw new Error('Not enough parking space.');
+        }
+
+        this.vehicles.push({carNumber, carModel, payed: false, toString: this.carToString});
+        return `The ${carModel}, with a registration number ${carNumber}, parked.`
+    }
+    removeCar(carNumber){
+        let found = this.vehicles.findIndex(c => c.carNumber == carNumber);
+
+        if(found == -1){
+            throw `The car, you're looking for, is not found.`;
+        }
+        if(!this.vehicles[found].payed){
+            return(`${carNumber} needs to pay before leaving the parking lot.`);
+        }else{
+            this.vehicles = this.vehicles.filter(c => c.carNumber != carNumber);
+            return `${carNumber} left the parking lot.`
+        }
+    }
+    pay(carNumber){
+        let found = this.vehicles.find(c => c.carNumber == carNumber);
+
+        if(!found){
+            throw `${carNumber} is not in the parking lot.`;
+        }
+
+        if(found.payed){
+            throw `${carNumber}'s driver has already payed his ticket.`;
+        }
+
+        found.payed = true;
+        return(`${carNumber}'s driver successfully payed for his stay.`);
+    }
+    getStatistics(carNumber){
+        if(carNumber){
+            return this.vehicles.find(c => c.carNumber == carNumber).toString();
+        }else{
+            return [`The Parking Lot has ${this.capacity - this.vehicles.length} empty spots left.`]
+                    .concat([...this.vehicles]
+                                        .sort((carA, carB) => carA.carModel.localeCompare(carB.carModel))
+                                        .map(c => c.toString())
+                    )
+                    .join('\n');
+        }
+    }
+
+    carToString() {
+        return `${this.carModel} == ${this.carNumber} - ${this.payed?'Has payed':'Not payed'}`;
+    }
+}
+
+
+const parking = new Parking(12);
+
+console.log(parking.addCar("Colvo t600", "TX3691CA"));
+parking.addCar('Zazka', 'ST1345SA');
+parking.addCar('Audi', 'ST1345S3');
+parking.addCar('BMW', 'ST1345Sf');
+parking.addCar('Giat', 'ST1345Sq');
+parking.pay('TX3691CA');
+parking.removeCar('TX3691CA')
+console.log(parking.getStatistics());
