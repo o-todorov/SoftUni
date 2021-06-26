71c971e6b058482a5e6fdeff36324dfe7886f7ea
o-todorov <todorov.oleg44@gmail.com>
2021-06-26 21:42:37 +0100

    Heroes - updated

--- JS Advanced/Exercises/03.Objects/Heroes.js
+++ JS Advanced/Exercises/03.Objects/Heroes.js
@@ -1,31 +1,28 @@
 function solve(){
-    let createhero = {};
-    createhero.fighter = function(name){
-        let hero = {
-            name,
-            health: 100,
-            stamina: 100,
-            fight: function(){
-                this.stamina--;
-                console.log(`${this.name} slashes at the foe!`);
+    return{ 
+        fighter(name){
+            return {
+                name,
+                health: 100,
+                stamina: 100,
+                fight(){
+                    this.stamina--;
+                    console.log(`${this.name} slashes at the foe!`);
+                }
             }
-        };
-        return hero;
-    }
-    createhero.mage = function(name){
-        let hero = {
-            name,
-            health: 100,
-            mana: 100,
-            cast: function(spell){
-                this.mana--;
-                console.log(`${this.name} cast ${spell}`);
+        },
+        mage(name){
+            return {
+                name,
+                health: 100,
+                mana: 100,
+                cast(spell){
+                    this.mana--;
+                    console.log(`${this.name} cast ${spell}`);
+                }
             }
-        };
-        return hero;
+        }
     }
-    
-    return createhero;
 }
 
 let create = solve();
