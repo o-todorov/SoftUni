1ff4473d3ca724a56b56b7a5f00ab93555fde851
o-todorov <todorov.oleg44@gmail.com>
2021-07-04 11:20:03 +0100

    updated

--- JS Advanced/Exercises/05.DOM-Events/06. Furniture/solution.js
+++ JS Advanced/Exercises/05.DOM-Events/06. Furniture/solution.js
@@ -2,9 +2,7 @@ function solve() {
   let buttons = document.querySelectorAll('#exercise button');
   buttons[0].addEventListener('click', generateItem);
   buttons[1].addEventListener('click', buyItem);
-  let textAreas = document.querySelectorAll('#exercise textarea');
-  let inputArea = textAreas[0];
-  let boughtArea = textAreas[1];
+  let [inputArea, boughtArea] = [...document.querySelectorAll('#exercise textarea')];
   let tbody = document.querySelector('tbody');
 
   function generateItem(){
@@ -13,27 +11,16 @@ function solve() {
           let tr = document.createElement('tr');
           let tds = Array.from({length:5}, () => document.createElement('td'));
 
-          let img = document.createElement('img');
-          img.src = item.img;
-
-          tds[0].appendChild(img);
-          tds[1].appendChild(createP(item.name));
-          tds[2].appendChild(createP(item.price));
-          tds[3].appendChild(createP(item.decFactor));
-
-          let checkbox = document.createElement('input');
-          checkbox.type = 'checkbox';
-          tds[4].appendChild(checkbox);
+          tds[0].appendChild(document.createElement('img')).src = item.img;
+          tds[1].appendChild(document.createElement('p')).textContent = item.name;
+          tds[2].appendChild(document.createElement('p')).textContent = item.price;
+          tds[3].appendChild(document.createElement('p')).textContent = item.decFactor;
+          tds[4].appendChild(document.createElement('input')).type = 'checkbox';
 
           tds.forEach((x) => tr.appendChild(x));
           tbody.appendChild(tr);
         });
-    function createP(value){
-      let p = document.createElement('p');
-      p.textContent = value;
-      return p;
-    }
-  }
+ }
 
   function buyItem(){
     let rows = Array.from(tbody.querySelectorAll('tr'))
@@ -47,8 +34,8 @@ function solve() {
     let totalDecFactor  = rows.reduce((t, item) => t + item.decFactor, 0);  
     let avgDecFactor    = totalDecFactor / rows.length;
 
-    boughtArea.value = `Bought furniture: ${names}\n`;
-    boughtArea.value += `Total price: ${totalPrice}\n`;
-    boughtArea.value += `Average decoration factor: ${avgDecFactor}`;
+    boughtArea.value = `Bought furniture: ${names}
+Total price: ${totalPrice}
+Average decoration factor: ${avgDecFactor}`;
   }
 }
